41bd55d589bcf3e648e3ee2989d92ed61cebac74
Charles Barto <chbarto@microsoft.com>
2019-07-30 13:55:40 -0700

    add documentation example

--- project_template/inc/az/exampleshortname.h
+++ project_template/inc/az/exampleshortname.h
@@ -1,3 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
+
+/** @file exampleshortname.h Azure example library */
 #include <az/exampleshortname/operations.h>
--- project_template/inc/az/exampleshortname/operations.h
+++ project_template/inc/az/exampleshortname/operations.h
@@ -1,12 +1,18 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
+
+/** @file operations.h */
 #pragma once
 #include <stdint.h>
 #include <az/exampleshortname/export.h>
 #ifdef __cplusplus
 extern "C" {
 #endif
-
+/** @brief add two numbers together
+ *  @param[in] a
+ *  @param[in] b
+ *  @return @p a + @p b
+ */
 AZ_EXAMPLESHORTNAME_EXPORT int32_t az_exampleshortname_add_two_numbers(int32_t a, int32_t b);
 
 #ifdef __cplusplus
