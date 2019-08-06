282a10c5f3c3d1b7a5efc9c8ffd3c053b4b5494a
Sergey Shandar <sergey.shandar@gmail.com>
2019-08-06 11:53:16 -0700

    core library template.

---
+++ sdk/core/core/inc/az_core.h
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#ifndef AZ_CORE_H
+#define AZ_CORE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ sdk/core/core/src/az_core.c
@@ -0,0 +1,4 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#include "../inc/az_core.h"
