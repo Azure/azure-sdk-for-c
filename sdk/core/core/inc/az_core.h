a3db2e208308c231b326c0f5121bcfd0969f2bf3
Sergey Shandar <sergey.shandar@gmail.com>
2019-08-06 11:58:45 -0700

    az_core

--- sdk/core/core/inc/az_core.h
+++ sdk/core/core/inc/az_core.h
@@ -8,6 +8,26 @@
 extern "C" {
 #endif
 
+// Range
+
+#define AZ_DEFINE_RANGE(TYPE, NAME) typedef struct { TYPE *begin; TYPE *end; } NAME;
+
+#define AZ_ARRAY_SIZE(A) (sizeof(A) / sizeof(*(A)))
+
+// String
+
+AZ_DEFINE_RANGE(char const, az_str);
+
+AZ_DEFINE_RANGE(char, az_mutable_str)
+
+#define AZ_STR_SIZE(S) (sizeof(S "") - 1)
+
+#define _AZ_DEFINE_STR(NAME, ARRAY, VALUE) \
+  static char const ARRAY[] = VALUE; \
+  static az_str NAME = { .begin = ARRAY, .end = ARRAY + AZ_STR_SIZE(VALUE) }
+
+#define AZ_DEFINE_STR(NAME, VALUE) _AZ_DEFINE_STR(NAME, _ ## NAME, VALUE)
+
 #ifdef __cplusplus
 }
 #endif
