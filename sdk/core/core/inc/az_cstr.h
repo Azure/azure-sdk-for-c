aaf35b72d275acd035bc49545623f1a08c96ac54
Sergey Shandar <sergey.shandar@gmail.com>
2019-08-07 10:55:25 -0700

    length

--- sdk/core/core/inc/az_cstr.h
+++ sdk/core/core/inc/az_cstr.h
@@ -4,35 +4,35 @@
 #ifndef AZ_CSTR_H
 #define AZ_CSTR_H
 
-#include <stdbool.h>
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 // A constant string.
-// The string is empty if `begin` is equal to `end`.
 typedef struct {
   // Points to the first character.
-  char const *begin;
-  // Points to the character after the last character.
-  char const *end;
+  char const *p;
+  // Length (in chars) of the string.
+  size_t len;
 } az_cstr;
 
 // A size of the string literal.
 // Details: to make sure that `S` is a `string literal`, we are adding `""` to `S`.
 #define AZ_STRING_LITERAL_SIZE(S) (sizeof(S "") - 1)
 
-#define _AZ_CSTR(NAME, ARRAY, STRING_LITERAL) \
-  static char const ARRAY[] = STRING_LITERAL; \
-  static az_cstr NAME = { .begin = ARRAY, .end = ARRAY + AZ_STRING_LITERAL_SIZE(STRING_LITERAL) };
-
 // Defined a new constant string `NAME` which is equalent to the `STRING_LITERAL`. For example:
 //
 // ```c
 // AZ_CSTR(hello_world, "Hello world!");
+//
+// for (size_t i = 0; i < hello_world.len; ++i) {
+//   ...hello_world.p[i]...
+// }
 // ```
-#define AZ_CSTR(NAME, STRING_LITERAL) _AZ_CSTR(NAME, _ ## NAME, STRING_LITERAL)
+#define AZ_CSTR(NAME, STRING_LITERAL) \
+  static az_cstr const NAME = { .begin = STRING_LITERAL, .size = AZ_STRING_LITERAL_SIZE(STRING_LITERAL) };
 
 #ifdef __cplusplus
 }
