65040349b15b69d6145447ad304ddebe396bbf20
Sergey Shandar <sergey.shandar@gmail.com>
2019-10-01 17:38:07 -0700

    dummy function.

--- sdk/core/core/inc/az_static_assert.h
+++ sdk/core/core/inc/az_static_assert.h
@@ -7,9 +7,9 @@
 
 #include <_az_cfg_prefix.h>
 
-inline void az_static_assert(volatile int x[1]) { x; }
+inline int az_static_assert(int x[1]) { return x[0]; }
 
-#define AZ_STATIC_ASSERT(CONDITION) inline void az_static_assert(volatile int x[(CONDITION) ? 1 : -1]);
+#define AZ_STATIC_ASSERT(CONDITION) inline int az_static_assert(int x[(CONDITION) ? 1 : -1]);
 
 AZ_STATIC_ASSERT(true)
 
