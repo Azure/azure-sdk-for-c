7d0f0590c909e1a65c62cd789421e1c4e7199ff4
Sergey Shandar <sergey.shandar@gmail.com>
2019-10-01 17:34:19 -0700

    another one

--- sdk/core/core/inc/az_static_assert.h
+++ sdk/core/core/inc/az_static_assert.h
@@ -7,9 +7,9 @@
 
 #include <_az_cfg_prefix.h>
 
-inline void az_static_assert(int x[1]) { x; }
+inline void az_static_assert(volatile int x[1]) { x; }
 
-#define AZ_STATIC_ASSERT(CONDITION) inline void az_static_assert(int x[(CONDITION) ? 1 : -1]);
+#define AZ_STATIC_ASSERT(CONDITION) inline void az_static_assert(volatile int x[(CONDITION) ? 1 : -1]);
 
 AZ_STATIC_ASSERT(true)
 
