3c1ae8eb0f83eb4dc14b2e13f2deeecd8166a97d
Sergey Shandar <sergey.shandar@gmail.com>
2019-10-01 17:17:44 -0700

    counter

--- sdk/core/core/inc/az_static_assert.h
+++ sdk/core/core/inc/az_static_assert.h
@@ -7,7 +7,7 @@
 
 #include <_az_cfg_prefix.h>
 
-#define AZ_STATIC_ASSERT(CONDITION) typedef int az_static_assert[(CONDITION) ? 1 : -1];
+#define AZ_STATIC_ASSERT(CONDITION) typedef int az_static_assert_##__COUNTER__[(CONDITION) ? 1 : -1];
 
 AZ_STATIC_ASSERT(true)
 
