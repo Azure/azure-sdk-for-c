dfce7290f618a76e40966520f2b75f772521284e
Anton Kolesnyk <antk@microsoft.com>
2019-10-04 00:39:54 -0700

    Initial version for the HTTPS stopgap (az_net.h)

---
+++ sdk/core/core/inc/az_net.h
@@ -0,0 +1,52 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#ifndef AZ_NET_H
+#define AZ_NET_H
+
+#include <az_result.h>
+#include <az_span.h>
+
+#include <stddef.h>
+
+#include <_az_cfg_prefix.h>
+
+typedef enum {
+  AZ_NET_HTTP_METHOD_POST = 0,
+  AZ_NET_HTTP_METHOD_GET = 1,
+} az_net_http_method;
+
+az_result az_net_invoke_rest_method(
+    az_net_http_method const http_method,
+    az_const_span const uri,
+    az_const_span const body,
+    az_const_span const headers,
+    az_span const response_buffer,
+    az_const_span * const out_response
+  );
+
+az_result az_net_uri_escape(
+    az_const_span const s,
+    az_span const buffer,
+    az_const_span * const out_result);
+
+static inline az_result az_net_uri_unescape(
+    az_const_span const s,
+    az_span const buffer,
+    az_const_span * const out_result) {
+  if (out_result == NULL) {
+    return AZ_ERROR_ARG;
+  }
+
+  {
+    // C4100: unreferenced formal parameter
+    s;
+    buffer;
+  }
+
+  return AZ_ERROR_NOT_IMPLEMENTED;
+}
+
+#include <_az_cfg_suffix.h>
+
+#endif
---
+++ sdk/core/core/src/az_net.c
@@ -0,0 +1,239 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#include <az_net.h>
+#include <az_static_assert.h>
+
+#include <assert.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <_az_cfg_warn.h>
+
+#ifdef _MSC_VER
+// warning C4996: 'strcpy/strcat': This function or variable may be unsafe. Consider using
+// strcpy_s/strcat_s instead.
+#pragma warning(disable : 4996)
+#endif
+
+static char const * alloc_shell_exec(char const * const cmd) {
+  assert(cmd != NULL);
+
+  FILE * const cmd_output = _popen(cmd, "r");
+  assert(cmd_output != NULL);
+
+  int seek_end_status = fseek(cmd_output, 0, SEEK_END);
+  assert(seek_end_status == 0);
+  size_t const output_length = (size_t)ftell(cmd_output);
+
+  long const rewind_pos = fseek(cmd_output, 0, SEEK_SET);
+  assert(rewind_pos == 0);
+
+  char * const result = calloc(output_length + 1, sizeof(char));
+  assert(result != NULL);
+
+  size_t const chars_read = fread(result, sizeof(char), output_length, cmd_output) / sizeof(char);
+  assert(chars_read == output_length);
+  assert(feof(cmd_output));
+
+  fclose(cmd_output);
+
+  return result;
+}
+
+static inline bool should_escape(char c) { return (c == '\"' || c == '\\'); }
+
+static char const * alloc_shell_string_escape(char const * const s) {
+  assert(s != NULL);
+  size_t result_strlen = 0;
+
+  size_t const input_strlen = strlen(s);
+  for (size_t i = 0; i < input_strlen; ++i) {
+    result_strlen += should_escape(s[i]) ? 2 : 1;
+  }
+
+  char * const result = calloc(result_strlen + 1, sizeof(char));
+  assert(result != NULL);
+  {
+    for (size_t i = 0, j = 0; i < input_strlen; ++i, ++j) {
+      char c = s[i];
+
+      if (should_escape(c)) {
+        result[j] = '\\';
+        ++j;
+      }
+
+      assert(j <= result_strlen);
+      result[j] = c;
+    }
+  }
+
+  return result;
+}
+
+static inline char const * alloc_concat_3_strings(
+    char const * const prefix,
+    char const * const body,
+    char const * const suffix) {
+  size_t result_size = strlen(prefix) + strlen(body) + strlen(suffix);
+
+  char * const result = calloc(result_size + 1, sizeof(char));
+  assert(result != NULL);
+
+  strcpy(result, prefix);
+  strcat(result, body);
+  strcat(result, suffix);
+
+  return result;
+}
+
+static inline char const * alloc_shell_exec_powershell(char const * const ps) {
+  char const * const escaped_ps = alloc_shell_string_escape(ps);
+  char const * const cmd = alloc_concat_3_strings("powershell \"", escaped_ps, "\"");
+
+  char const * const result = alloc_shell_exec(cmd);
+
+  free((void *)escaped_ps);
+  free((void *)cmd);
+
+  return result;
+}
+
+static inline char const * alloc_c_str(az_const_span const span) {
+  AZ_STATIC_ASSERT(sizeof(char) == sizeof(*span.begin));
+
+  char * const result = calloc(span.size + 1, sizeof(char));
+  assert(result != NULL);
+  memcpy(result, span.begin, span.size * sizeof(char));
+
+  return result;
+}
+
+static inline az_result copy_to_span(
+    const char * const s,
+    az_span const span,
+    az_const_span * const out_result) {
+  AZ_STATIC_ASSERT(sizeof(char) == sizeof(*span.begin));
+
+  assert(out_result != NULL);
+  assert(span.begin == out_result->begin);
+  assert(span.size == out_result->size);
+
+  size_t s_strlen = strlen(s);
+  if (s_strlen > span.size) {
+    return AZ_ERROR_NO_BUFFER_SPACE;
+  }
+
+  memcpy(out_result, s, s_strlen * sizeof(char));
+  out_result->size = s_strlen;
+
+  return AZ_OK;
+}
+
+az_result az_net_uri_escape(
+    az_const_span const s,
+    az_span const buffer,
+    az_const_span * const out_result) {
+  if (out_result == NULL) {
+    return AZ_ERROR_ARG;
+  }
+
+  char const * param = alloc_c_str(s);
+  char const * script = alloc_concat_3_strings("[uri]::EscapeDataString(\"", param, "\")");
+
+  char const * escaped = alloc_shell_exec_powershell(script);
+
+  az_result const result = copy_to_span(escaped, buffer, out_result);
+
+  free((void *)param);
+  free((void *)script);
+  free((void *)escaped);
+
+  return result;
+}
+
+az_result az_net_invoke_rest_method(
+    az_net_http_method const http_method,
+    az_const_span const uri,
+    az_const_span const body,
+    az_const_span const headers,
+    az_span const response_buffer,
+    az_const_span * const out_response) {
+  if (uri.size < sizeof("https://x")) {
+    return AZ_ERROR_ARG;
+  }
+
+  char const * http_method_str = NULL;
+  switch (http_method) {
+    case AZ_NET_HTTP_METHOD_POST:
+      http_method_str = "Post";
+      break;
+
+    case AZ_NET_HTTP_METHOD_GET:
+      http_method_str = "Get";
+      break;
+
+    default:
+      return AZ_ERROR_NOT_IMPLEMENTED;
+  }
+
+  char const * cmd = NULL;
+  {
+    char const * cmd_part1 = "Invoke-RestMethod -Method ";
+    char const * cmd_part2 = " -Uri '";
+    char const * cmd_part34_body = "' -Body '";
+    char const * cmd_part34_headers = "' -Headers '";
+    char const * cmd_part45_end = "' | ConvertTo-Json -Compress";
+
+    bool has_body = (body.size > 0);
+    bool has_headers = (headers.size > 0);
+
+    cmd = alloc_concat_3_strings(cmd_part1, http_method_str, cmd_part2);
+
+    {
+      char const * next_part = cmd_part45_end;
+      if (has_body) {
+        next_part = cmd_part34_body;
+      } else if (has_headers) {
+        next_part = cmd_part34_headers;
+      }
+
+      char const * const uri_str = alloc_c_str(uri);
+      char const * const cmd_old = cmd;
+
+      cmd = alloc_concat_3_strings(cmd, uri_str, next_part);
+
+      free((void *)cmd_old);
+      free((void *)uri_str);
+    }
+
+    if (has_body) {
+      char const * const body_str = alloc_c_str(body);
+      char const * const cmd_old = cmd;
+
+      cmd = alloc_concat_3_strings(
+          cmd, body_str, has_headers ? cmd_part34_headers : cmd_part45_end);
+
+      free((void *)cmd_old);
+      free((void *)body_str);
+    }
+
+    if (has_headers) {
+      char const * const headers_str = alloc_c_str(headers);
+      char const * const cmd_old = cmd;
+
+      cmd = alloc_concat_3_strings(cmd, headers_str, cmd_part45_end);
+
+      free((void *)cmd_old);
+      free((void *)headers_str);
+    }
+  }
+
+  char const * json_str = alloc_shell_exec_powershell(cmd);
+  az_result const result = copy_to_span(json_str, response_buffer, out_response);
+  free((void *)json_str);
+
+  return result;
+}
