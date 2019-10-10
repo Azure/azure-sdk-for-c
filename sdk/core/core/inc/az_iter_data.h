f2860c9f48d974919ee569bc1dbaf2792e5b8335
Sergey Shandar <sergey.shandar@gmail.com>
2019-10-10 14:15:52 -0700

    Comments.

--- sdk/core/core/inc/az_iter_data.h
+++ sdk/core/core/inc/az_iter_data.h
@@ -6,6 +6,23 @@
 
 #include <_az_cfg_prefix.h>
 
+/**
+ * Generic data for any polymorphic iterator.
+ *
+ * The assumption is that one pointer points to an immutable container and another one is used
+ * for iteration (mutable field). 
+ *
+ * Examples:
+ * - span iterator:
+ *   - @begin points to the first item (this value is mutable)
+ *   - @end points to the item after the last one (this value is immutable)
+ * - linked list:
+ *   - @begin points to the first item of a list.
+ *   - @end is NULL
+ * - FILE
+ *   - @begin is a FILE HANDLE
+ *   - @end is a position in the file.
+ */
 typedef struct {
   void const * begin;
   void const * end;
