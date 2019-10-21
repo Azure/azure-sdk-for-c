acab69c9a8cb3dfce45b24b13a4f72ccfb6593e1
Victor Vazquez <vhvb1989@gmail.com>
2019-10-21 20:35:37 +0000

    completed keyVault POC with curl

--- sdk/core/core/inc/az_http_client.h
+++ sdk/core/core/inc/az_http_client.h
@@ -12,8 +12,16 @@
 
 #include <_az_cfg_prefix.h>
 
-AZ_INLINE az_result az_http_client_send_request(az_http_request const * const p_request) {
-  return az_send_request_impl(p_request);
+/**
+ * @brief Passing http request to adapter. Adapter will handle GET, POST, PUT or DELETE
+ *
+ * @param p_request pointer to method, headers, body and valid url to make a request
+ * @param response pointer to span where response will be written
+ * @return AZ_INLINE az_http_client_send_request
+ */
+AZ_INLINE az_result
+az_http_client_send_request(az_http_request const * const p_request, az_span * const response) {
+  return az_send_request_impl(p_request, response);
 }
 
 #include <_az_cfg_suffix.h>
--- sdk/core/core/inc/az_span.h
+++ sdk/core/core/inc/az_span.h
@@ -8,7 +8,6 @@
 #include <az_contract.h>
 #include <az_result.h>
 #include <az_static_assert.h>
-#include <az_callback.h>
 
 #include <limits.h>
 #include <stdbool.h>
