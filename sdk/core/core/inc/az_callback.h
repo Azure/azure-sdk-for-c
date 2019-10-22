5e421d9e29f542bb7380371712df966479e2ade6
Sergey Shandar <sergey.shandar@gmail.com>
2019-10-22 00:03:15 -0700

    Some comments.

--- sdk/core/core/inc/az_callback.h
+++ sdk/core/core/inc/az_callback.h
@@ -12,6 +12,9 @@ typedef void * az_callback_data;
 
 #define AZ_CALLBACK_ARG(NAME) AZ_CAT(NAME, _arg)
 
+/**
+ * Defines a callback @NAME type which accepts an argument of type @ARG.
+ */
 #define AZ_CALLBACK_TYPE(NAME, ARG) \
   typedef ARG AZ_CALLBACK_ARG(NAME); \
   typedef struct { \
@@ -19,6 +22,10 @@ typedef void * az_callback_data;
     az_callback_data data; \
   } NAME;
 
+/**
+ * Defines a function @NAME##_callback which creates a callback of type @CALLBACK
+ * using the given @NAME function.
+ */
 #define AZ_CALLBACK_FUNC(NAME, DATA, CALLBACK) \
   AZ_STATIC_ASSERT(sizeof(DATA) <= sizeof(az_callback_data)) \
   az_result NAME(DATA const, AZ_CALLBACK_ARG(CALLBACK) const); \
