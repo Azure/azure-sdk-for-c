0a76bf44da8601b00494e7f3bf18fb196206978d
Victor Vazquez <vhvb1989@gmail.com>
2019-10-23 16:08:40 +0000

    restore http client

---
+++ sdk/core/core/inc/az_http_client.h
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#ifndef AZ_HTTP_CLIENT_H
+#define AZ_HTTP_CLIENT_H
+
+#ifdef MOCK_CURL
+#include <az_mock_curl.h>
+#else
+#include <az_curl_adapter.h>
+#endif
+
+#include <_az_cfg_prefix.h>
+
+/**
+ * @brief Passing http request to adapter. Adapter will handle GET, POST, PUT or DELETE
+ *
+ * @param p_hrb pointer to method, headers, body and valid url to make a request
+ * @param response pointer to span where response will be written
+ * @return AZ_INLINE az_http_client_send_request
+ */
+AZ_INLINE az_result
+az_http_client_send_request(az_http_request const * const p_hrb, az_span * const response) {
+  return az_send_request_impl(p_hrb, response);
+}
+
+#include <_az_cfg_suffix.h>
+
+#endif
