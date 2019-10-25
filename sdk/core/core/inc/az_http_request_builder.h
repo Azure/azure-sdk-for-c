b1d0fd6c6d32e7ab7944fffe80fbdf289d37f3ed
antkmsft <41349689+antkmsft@users.noreply.github.com>
2019-10-24 17:30:38 -0700

    Clean up code (#114)

--- sdk/core/core/inc/az_http_request_builder.h
+++ sdk/core/core/inc/az_http_request_builder.h
@@ -2,20 +2,17 @@
 // SPDX-License-Identifier: MIT
 
 /**
- * @file az_http_request_builder.h
- * @author your name (you@domain.com)
  * @brief Interface declaration for bulding an HTTP Request.
- * @date 2019-10-23
- *
  */
 
 #ifndef AZ_HTTP_REQUEST_BUILDER_H
 #define AZ_HTTP_REQUEST_BUILDER_H
 
-#include <_az_cfg_prefix.h>
-
 #include <az_pair.h>
+#include <az_result.h>
 #include <az_span.h>
+
+#include <_az_cfg_prefix.h>
 
 typedef struct {
   az_span buffer;
@@ -27,14 +24,7 @@ typedef struct {
 } az_http_request_builder;
 
 /**
- * @brief format buffer as a http request containing headers, url and body. Url max size is
- * initially set to a default size. use `az_http_request_builder_set_max_url_size` after init to
- * change this value if required
- *
- * @param out
- * @param buffer
- * @param method_verb
- * @return az_result
+ * @brief format buffer as a http request containing headers, url and body.
  */
 az_result az_http_request_builder_init(
     az_http_request_builder * const p_hrb,
@@ -46,11 +36,6 @@ az_result az_http_request_builder_init(
 /**
  * @brief set a query parameter. If the query name is not in url yet, it will be added, otherwise
  * modified
- *
- * @param p_hrb
- * @param name
- * @param value
- * @return az_result
  */
 az_result az_http_request_builder_set_query_parameter(
     az_http_request_builder * const p_hrb,
@@ -59,39 +44,16 @@ az_result az_http_request_builder_set_query_parameter(
 
 /**
  * @brief add a new header for the request.
- *
- * @param p_hrb
- * @param name
- * @param value
- * @return az_result
  */
 az_result az_http_request_builder_append_header(
     az_http_request_builder * const p_hrb,
     az_const_span const name,
     az_const_span const value);
 
-/**
- * @brief
- *
- * @param p_hrb
- * @return az_result
- */
 az_result az_http_request_builder_mark_retry_headers_start(az_http_request_builder * const p_hrb);
 
-/**
- * @brief
- *
- * @param p_hrb
- * @return az_result
- */
 az_result az_http_request_builder_remove_retry_headers(az_http_request_builder * const p_hrb);
 
-/**
- * @brief
- *
- * @param p_hrb
- * @return az_result
- */
 az_result az_http_request_builder_remove_retry_headers(az_http_request_builder * const p_hrb);
 
 #include <_az_cfg_suffix.h>
--- sdk/core/core/src/az_http_request_builder.c
+++ sdk/core/core/src/az_http_request_builder.c
@@ -1,27 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
 
-/**
- * @file az_http_request_builder.c
- * @author https://github.com/Azure/azure-sdk-for-c
- * @brief Interface implementation for bulding an HTTP Request. Bulder maintans the state of a
- * control block for writing request into a buffer
- * @date 2019-10-23
- *
- */
-
 #include <az_http_request_builder.h>
 
-/**
- * @brief
- *
- * @param p_hrb
- * @param buffer
- * @param max_url_size
- * @param method_verb
- * @param initial_url
- * @return az_result
- */
+#include <_az_cfg.h>
+
 az_result az_http_request_builder_init(
     az_http_request_builder * const p_hrb,
     az_span const buffer,
@@ -36,14 +19,6 @@ az_result az_http_request_builder_init(
   return AZ_OK;
 }
 
-/**
- * @brief
- *
- * @param p_hrb
- * @param name
- * @param value
- * @return az_result
- */
 az_result az_http_request_builder_set_query_parameter(
     az_http_request_builder * const p_hrb,
     az_const_span const name,
@@ -54,14 +29,6 @@ az_result az_http_request_builder_set_query_parameter(
   return AZ_OK;
 }
 
-/**
- * @brief
- *
- * @param p_hrb
- * @param name
- * @param value
- * @return az_result
- */
 az_result az_http_request_builder_append_header(
     az_http_request_builder * const p_hrb,
     az_const_span const name,
@@ -72,23 +39,11 @@ az_result az_http_request_builder_append_header(
   return AZ_OK;
 }
 
-/**
- * @brief
- *
- * @param p_hrb
- * @return az_result
- */
 az_result az_http_request_builder_mark_retry_headers_start(az_http_request_builder * const p_hrb) {
   (void)p_hrb;
   return AZ_OK;
 }
 
-/**
- * @brief
- *
- * @param p_hrb
- * @return az_result
- */
 az_result az_http_request_builder_remove_retry_headers(az_http_request_builder * const p_hrb) {
   (void)p_hrb;
   return AZ_OK;
