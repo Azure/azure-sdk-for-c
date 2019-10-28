c139ba95c25e0894a66f4edecc7062450a797f88
Sergey Shandar <sergey.shandar@gmail.com>
2019-10-28 06:55:09 -0700

    az_span_action

--- sdk/core/core/inc/az_span_emitter.h
+++ sdk/core/core/inc/az_span_emitter.h
@@ -14,16 +14,13 @@ AZ_ACTION_TYPE(az_span_emitter, az_span_action)
 /**
  * A span of spans of bytes.
  */
-typedef struct {
-  az_span const * begin;
-  size_t size;
-} az_span_span;
+AZ_SPAN_TYPE(az_span_span, az_span const)
 
 /**
- * Emits all spans from `*p_span_span` into @append.
+ * Emits all spans from @self into @append.
  */
 AZ_NODISCARD az_result
-az_span_span_emit(az_span_span const * const p_span_span, az_span_action const action);
+az_span_span_emit(az_span_span const * const self, az_span_action const action);
 
 /**
  * @az_span_span_emit as a callback.
@@ -31,14 +28,15 @@ az_span_span_emit(az_span_span const * const p_span_span, az_span_action const a
 AZ_ACTION_FUNC(az_span_span_emit, az_span_span const, az_span_emitter)
 
 /**
- * Calculates a size of a contigous buffer to store all spans from @seq.
+ * Calculates a size of a contigous buffer to store all spans from @self.
  */
-AZ_NODISCARD az_result az_span_emitter_size(az_span_emitter const emitter, size_t * const out_size);
+AZ_NODISCARD az_result az_span_emitter_size(az_span_emitter const self, size_t * const out_size);
 
 /**
  * The function creates a temporary zero-terminated string from @emmiter in dynamic memory.
- * The string is passed to the given @callback. After @callback is returned, the temporary string is
- * destroyed.
+ * The string is passed to the given @str_action. 
+ * 
+ * Note: After @str_action is returned, the temporary string is destroyed.
  */
 AZ_NODISCARD az_result
 az_span_emitter_to_tmp_str(az_span_emitter const emitter, az_str_action const str_action);
