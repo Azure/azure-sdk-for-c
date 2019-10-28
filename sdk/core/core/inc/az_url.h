eff5b0a4b5fed43a88107c6189625889e46492d7
Sergey Shandar <sergey.shandar@gmail.com>
2019-10-28 06:31:16 -0700

    fix for az_url

--- sdk/core/core/inc/az_url.h
+++ sdk/core/core/inc/az_url.h
@@ -13,9 +13,9 @@
  * https://en.wikipedia.org/wiki/URL
  */
 typedef struct {
-  az_const_span host;
-  az_const_span path;
-  az_pair_seq query;
+  az_span host;
+  az_span path;
+  az_pair_emitter query;
 } az_url;
 
 AZ_NODISCARD az_result az_url_emit_span_seq(az_url const * const self, az_span_action const action);
