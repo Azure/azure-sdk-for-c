799056a2c6f016ea6a85669bdca286666c8dcf8b
Sergey Shandar <sergey.shandar@gmail.com>
2019-10-28 23:10:58 -0700

    formatting

--- sdk/core/core/src/az_curl_slist.c
+++ sdk/core/core/src/az_curl_slist.c
@@ -3,9 +3,9 @@
 
 #include <az_curl_slist.h>
 
-#include <az_http_result.h>
 #include <az_action.h>
 #include <az_http_header.h>
+#include <az_http_result.h>
 
 #include <_az_cfg.h>
 
@@ -26,18 +26,19 @@ az_curl_slist_append(struct curl_slist ** const self, char const * const str) {
 }
 
 /**
- * Creates a function `az_curl_slist_append_action(struct curlt_slist **)` 
+ * Creates a function `az_curl_slist_append_action(struct curlt_slist **)`
  * that returns a value of type `az_str_action`.
  */
 AZ_ACTION_FUNC(az_curl_slist_append, struct curl_slist *, az_str_action)
 
-AZ_NODISCARD az_result az_curl_slist_append_header(struct curl_slist ** const self, az_pair const header) {
+AZ_NODISCARD az_result
+az_curl_slist_append_header(struct curl_slist ** const self, az_pair const header) {
   AZ_CONTRACT_ARG_NOT_NULL(self);
 
   az_span_emitter const header_span_emitter = az_http_header_emit_span_seq_action(&header);
   az_str_action const curl_slist_append_action = az_curl_slist_append_action(self);
 
-  // the function creates a temporary dynamic zero-terminated string from `header_span_emitter` 
+  // the function creates a temporary dynamic zero-terminated string from `header_span_emitter`
   // and passes it to `curl_slist_append_action`.
   return az_span_emitter_to_tmp_str(header_span_emitter, curl_slist_append_action);
 }
