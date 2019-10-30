fe3e95a59679f02083654ec64b04238e85eea378
Sergey Shandar <sergey.shandar@gmail.com>
2019-10-29 22:55:24 -0700

    TODO:

--- sdk/core/core/src/az_http_header.c
+++ sdk/core/core/src/az_http_header.c
@@ -10,6 +10,9 @@ az_http_header_emit_span_seq(az_pair const * const self, az_span_action const ac
   AZ_CONTRACT_ARG_NOT_NULL(self);
 
   AZ_RETURN_IF_FAILED(az_span_action_do(action, self->key));
+  // TODO: it looks like we may no need space. 
+  // Probably ":" would be sufficient.
+  // We need to check RFC and CURL SLIST.
   AZ_RETURN_IF_FAILED(az_span_action_do(action, AZ_STR(": ")));
   AZ_RETURN_IF_FAILED(az_span_action_do(action, self->value));
   return AZ_OK;
