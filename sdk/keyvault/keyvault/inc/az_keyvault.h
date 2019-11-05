6fd79fdbdfecfd84321132641c8cf54a77be1165
Victor Vazquez <vhvb1989@gmail.com>
2019-11-05 22:47:27 +0000

    compiliing version of keyvault client

--- sdk/keyvault/keyvault/inc/az_keyvault.h
+++ sdk/keyvault/keyvault/inc/az_keyvault.h
@@ -4,22 +4,30 @@
 #ifndef AZ_KEYVAULT_H
 #define AZ_KEYVAULT_H
 
+#include <az_contract.h>
+#include <az_result.h>
+#include <az_span.h>
+
+#include <stdlib.h>
+
 #include <_az_cfg_prefix.h>
 
 typedef struct {
   az_span uri;
-  //TODO: Azure Credentials
+  // TODO: Azure Credentials
 } az_keyvault_keys_client;
 
 typedef struct {
-  //API version
-  //Pipeline configuration options
+  // API version
+  // Pipeline configuration options
+  az_span version;
 } az_keyvault_keys_client_options;
 
 typedef struct {
-  //key size(size_t, typical default is 4096) 
-  //Expires date 
-  //All the REST options
+  // key size(size_t, typical default is 4096)
+  // Expires date
+  // All the REST options
+  az_span option;
 } az_keyvault_keys_keys_options;
 
 #include <_az_cfg_suffix.h>
