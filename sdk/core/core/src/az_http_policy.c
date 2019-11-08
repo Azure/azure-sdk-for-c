e2ee70d57a80d4708bb12c860f8bf645669c0a2c
Victor Vazquez <vhvb1989@gmail.com>
2019-11-08 21:30:59 +0000

    transport policy update

--- sdk/core/core/src/az_http_policy.c
+++ sdk/core/core/src/az_http_policy.c
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
 
+#include <az_http_client.h>
 #include <az_http_pipeline.h>
 #include <az_http_policy.h>
 
@@ -108,13 +109,14 @@ AZ_NODISCARD az_result az_http_pipeline_policy_transport(
   AZ_CONTRACT_ARG_NOT_NULL(p_policies);
   AZ_CONTRACT_ARG_NOT_NULL(hrb);
   AZ_CONTRACT_ARG_NOT_NULL(response);
-  // Make the actual request
 
   // Transport policy is the last policy
   //  If a policy exists after the transport policy
   if (p_policies[0].pfnc_process != NULL) {
     return AZ_ERROR_HTTP_PIPELINE_INVALID_POLICY;
   }
+
+  return az_http_client_send_request(hrb, response);
 
   return AZ_OK;
 }
