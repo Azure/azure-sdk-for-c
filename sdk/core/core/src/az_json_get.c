fa7f2cd9243adb7643bf46a69a9915e598abd439
Sergey Shandar <sergey.shandar@gmail.com>
2019-11-09 06:46:30 -0800

    some comments.

--- sdk/core/core/src/az_json_get.c
+++ sdk/core/core/src/az_json_get.c
@@ -44,6 +44,8 @@ az_json_get_object_member(az_span const json, az_span const name, az_json_value 
     while (true) {
       az_json_member member = { 0 };
       AZ_RETURN_IF_FAILED(az_json_parser_get_object_member(&parser, &member));
+      // TODO: we should either replace `az_span_eq` with something else or 
+      //       remove `az_json_get_object_member` completely.
       if (az_span_eq(member.name, name)) {
         *out_value = member.value;
         return AZ_OK;
