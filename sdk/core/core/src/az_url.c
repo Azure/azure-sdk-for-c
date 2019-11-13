448c3918c1b6dd0f1affc296c56c3e2453bfd5b7
Sergey Shandar <sergey.shandar@gmail.com>
2019-11-13 11:21:36 -0800

    URL

--- sdk/core/core/src/az_url.c
+++ sdk/core/core/src/az_url.c
@@ -192,19 +192,56 @@ az_span_reader_read_url_fragment(az_span_reader * const self, az_span * const fr
 
 /**
  * https://tools.ietf.org/html/rfc3986#section-3.2
+ *
+ * authority   = [ userinfo "@" ] host [ ":" port ]
  */
 AZ_NODISCARD az_result
 az_span_reader_read_url_authority(az_span_reader * const self, az_url_authority * const out) {
   AZ_CONTRACT_ARG_NOT_NULL(self);
   AZ_CONTRACT_ARG_NOT_NULL(out);
 
+  bool has_userinfo = false;
+  *out = (az_url_authority){ 0 };
   size_t begin = self->i;
   while (true) {
-    az_result_byte const c = az_span_reader_current(self);
+    az_result_byte c = az_span_reader_current(self);
+    AZ_RETURN_IF_FAILED(c);
     switch (c) {
-      case AZ_ERROR_EOF: {
+      case '@': {
+        if (has_userinfo) {
+          return AZ_ERROR_PARSER_UNEXPECTED_CHAR;
+        }
+        has_userinfo = true;
+        out->userinfo = az_span_sub(self->span, begin, self->i);
+        az_span_reader_next(self);
+        begin = self->i;
+        break;
       }
-      
+      case AZ_ERROR_EOF:
+      case '?':
+      case '/':
+      case '#': {
+        out->host = az_span_sub(self->span, begin, self->i);
+        return AZ_OK;
+      }
+      case ':': {
+        out->host = az_span_sub(self->span, begin, self->i);
+        az_span_reader_next(self);
+        begin = self->i;
+        while (true) {
+          c = az_span_reader_current(self);
+          if (!isdigit(c)) {
+            break;
+          }
+          az_span_reader_next(self);
+        }
+        out->port = az_span_sub(self->span, begin, self->i);
+        return AZ_OK;
+      }
+      default: {
+        az_span_reader_next(self);
+        break;
+      }
     }
   }
 
