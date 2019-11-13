3b9687c0dcc8ab0ffbee30abaa3bbe92f623793d
Sergey Shandar <sergey.shandar@gmail.com>
2019-11-12 17:24:37 -0800

    Scheme

--- sdk/core/core/src/az_url.c
+++ sdk/core/core/src/az_url.c
@@ -3,14 +3,38 @@
 
 #include <az_url.h>
 
+#include <az_str.h>
+#include <az_span_reader.h>
 #include <az_http_query.h>
 
 #include <_az_cfg.h>
 
+/**
+ * https://tools.ietf.org/html/rfc3986#section-3.1
+ */
+AZ_NODISCARD az_result
+az_span_reader_read_url_scheme(az_span_reader * const self, az_span * const out) {
+  AZ_CONTRACT_ARG_NOT_NULL(self);
+  AZ_CONTRACT_ARG_NOT_NULL(out);
+
+  while (true) {
+    az_result_byte const c = az_span_reader_current(self);
+    AZ_RETURN_IF_FAILED(c);
+    az_span_reader_next(self);
+    if (c == ':') {
+      *out = az_span_take(self->span, self->i);
+      AZ_RETURN_IF_FAILED(az_span_reader_expect_span(self, AZ_STR("//")));
+      return AZ_OK;
+    }
+  }
+}
+
 AZ_NODISCARD az_result az_url_parse(az_span const url, az_url * const out) {
   AZ_CONTRACT_ARG_NOT_NULL(out);
 
-  (void)url;
+  az_span_reader reader = az_span_reader_create(url);
+
+  AZ_RETURN_IF_FAILED(az_span_reader_read_url_scheme(&reader, &out->scheme));
 
   return AZ_ERROR_NOT_IMPLEMENTED;
 }
