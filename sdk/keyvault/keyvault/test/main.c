7b3e0cc8e4c86f9cc5674b732abd105c786a2789
Victor Vazquez <vhvb1989@gmail.com>
2019-11-19 19:31:55 +0000

    addding unit test for internal functions

--- sdk/keyvault/keyvault/test/main.c
+++ sdk/keyvault/keyvault/test/main.c
@@ -1,10 +1,32 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
 
-#include <az_keyvault.h>
+#include "../src/az_keyvault_client.c"
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "./az_test.h"
 
 #include <_az_cfg.h>
 
 int exit_code = 0;
 
-int main() { return exit_code; }
+int main() {
+  {
+    az_span const uri = AZ_STR("http://example.net");
+    az_span const key_type = AZ_STR("keys");
+    az_span const key_name = AZ_STR("name");
+    uint8_t out_array[50];
+    az_mut_span const out
+        = (az_mut_span){ .begin = out_array, .size = uri.size + key_name.size + key_type.size + 2 };
+
+    az_span const expected = AZ_STR("http://example.net/keys/name");
+
+    TEST_ASSERT(az_keyvault_build_url(uri, key_type, key_name, out) == AZ_OK);
+    TEST_ASSERT(az_span_eq(az_mut_span_to_span(out), expected));
+  }
+  return exit_code;
+}
