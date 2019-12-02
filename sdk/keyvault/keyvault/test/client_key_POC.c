905640f288b0e05d6368bcdfab186fc7c1ccd0b8
Victor Vazquez <vhvb1989@gmail.com>
2019-12-02 18:54:37 +0000

    enhance POC to get key by version

--- sdk/keyvault/keyvault/test/client_key_POC.c
+++ sdk/keyvault/keyvault/test/client_key_POC.c
@@ -16,8 +16,9 @@
 
 int exit_code = 0;
 
-az_span get_key_version(az_http_response * response, az_span * body) {
+az_span get_key_version(az_http_response * response) {
   az_http_response_parser parser = { 0 };
+  az_span body = { 0 };
   az_result r = az_http_response_parser_init(&parser, az_span_builder_result(&response->builder));
 
   az_http_response_status_line status_line = { 0 };
@@ -25,15 +26,25 @@ az_span get_key_version(az_http_response * response, az_span * body) {
 
   // Get Body
   r = az_http_response_parser_skip_headers(&parser);
-  r = az_http_response_parser_read_body(&parser, body);
+  r = az_http_response_parser_read_body(&parser, &body);
 
   // get key from body
   az_json_value value;
-  r = az_json_get_object_member(*body, AZ_STR("key"), &value);
-  r = az_json_get_object_member(*body, AZ_STR("kid"), &value);
+  r = az_json_get_by_pointer(body, AZ_STR("/key/kid"), &value);
 
   az_span k = { 0 };
   r = az_json_value_get_string(&value, &k);
+
+  // calculate version
+  for (uint8_t index = 0; index < k.size;) {
+    ++index;
+    if (*(k.begin + k.size - index) == '/') {
+      --index;
+      k.begin = k.begin + k.size - index;
+      k.size = index;
+      break;
+    }
+  }
 
   return k;
 }
@@ -65,7 +76,7 @@ int main() {
   az_result create_result = az_keyvault_keys_key_create(
       &client, AZ_STR("test-new-key"), AZ_KEYVAULT_JSON_WEB_KEY_TYPE_RSA, NULL, &http_response);
 
-  printf("Key created result: %d\n%s", create_result, response_buffer);
+  printf("Key created result: \n%s", response_buffer);
 
   // Reuse response buffer for create Key by creating a new span from response_buffer
   az_result reset_op = az_http_response_reset(&http_response);
@@ -75,22 +86,44 @@ int main() {
   az_result get_key_result
       = az_keyvault_keys_key_get(&client, AZ_STR("test-new-key"), AZ_SPAN_NULL, &http_response);
 
-  printf("Get Key result: %d\n%s", get_key_result, response_buffer);
+  printf("\n\n*********************************\nGet Key result: \n%s", response_buffer);
 
-  /****************** get key version  ****/
-  uint8_t body[1024 * 2];
-  az_span body_span = AZ_SPAN_FROM_ARRAY(body);
-  az_span const r = get_key_version(&http_response, &body_span);
+  /****************** get key version from response ****/
+  az_span version = get_key_version(&http_response);
+  // version is still at http_response. Let's copy it to a new buffer
+  uint8_t version_buf[40];
+  az_span_builder version_builder
+      = az_span_builder_create((az_mut_span)AZ_SPAN_FROM_ARRAY(version_buf));
+  az_result ap_res = az_span_builder_append(&version_builder, version);
+  version = az_span_builder_result(&version_builder);
 
   // Reuse response buffer for delete Key by creating a new span from response_buffer
   reset_op = az_http_response_reset(&http_response);
   az_mut_span_memset(http_response.builder.buffer, '.');
 
+  /*********************  Create a new key version  *************/
+  az_result create_version_result = az_keyvault_keys_key_create(
+      &client, AZ_STR("test-new-key"), AZ_KEYVAULT_JSON_WEB_KEY_TYPE_RSA, NULL, &http_response);
+
+  printf(
+      "\n\n*********************************\nKey new version created result: \n%s",
+      response_buffer);
+
+  // Reuse response buffer for delete Key by creating a new span from response_buffer
+  reset_op = az_http_response_reset(&http_response);
+  az_mut_span_memset(http_response.builder.buffer, '.');
+
+  /******************  GET KEY previous ver ******************************/
+  az_result get_key_prev_ver_result
+      = az_keyvault_keys_key_get(&client, AZ_STR("test-new-key"), version, &http_response);
+
+  printf("\n\n*********************************\nGet Key previous Ver: \n%s", response_buffer);
+
   /******************  DELETE KEY ******************************/
   az_result delete_key_result
       = az_keyvault_keys_key_delete(&client, AZ_STR("test-new-key"), &http_response);
 
-  printf("\n\nDELETED Key: %d\n %s", delete_key_result, response_buffer);
+  printf("\n\n*********************************\nDELETED Key: \n %s", response_buffer);
 
   // Reuse response buffer for create Key by creating a new span from response_buffer
   reset_op = az_http_response_reset(&http_response);
@@ -100,7 +133,9 @@ int main() {
   az_result get_key_again_result
       = az_keyvault_keys_key_get(&client, AZ_STR("test-new-key"), AZ_SPAN_NULL, &http_response);
 
-  printf("\n\nGet Key again after DELETE result: %d\n%s\n", get_key_again_result, response_buffer);
+  printf(
+      "\n\n*********************************\nGet Key again after DELETE result: \n%s\n",
+      response_buffer);
 
   return exit_code;
 }
