bc64e2dd4aff6a972b1f3f406bb525f2acec9ab4
Anton Kolesnyk <antk@microsoft.com>
2020-01-08 14:48:08 -0800

    Add log classifications

---
+++ sdk/core/core/inc/az_log.h
@@ -0,0 +1,26 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#ifndef _az_FACILITY_H
+#define _az_FACILITY_H
+
+#include <az_facility.h>
+
+#include <stdint.h>
+
+#include <_az_cfg_prefix.h>
+
+#define _az_LOG_MAKE_CLASSIFICATION(facility, code) \
+  ((int32_t)((uint32_t)(facility) << 16) | (uint32_t)(code))
+
+typedef enum {
+  AZ_LOG_ERROR = _az_LOG_MAKE_CLASSIFICATION(AZ_FACILITY_CORE, 1),
+  AZ_LOG_REQUEST = _az_LOG_MAKE_CLASSIFICATION(AZ_FACILITY_CORE, 2),
+  AZ_LOG_RESPONSE = _az_LOG_MAKE_CLASSIFICATION(AZ_FACILITY_CORE, 3),
+  AZ_LOG_RETRY_POLICY = _az_LOG_MAKE_CLASSIFICATION(AZ_FACILITY_CORE, 4),
+  AZ_LOG_SLOW_RESPONSE = _az_LOG_MAKE_CLASSIFICATION(AZ_FACILITY_CORE, 5),
+} az_log_classification;
+
+#include <_az_cfg_suffix.h>
+
+#endif
