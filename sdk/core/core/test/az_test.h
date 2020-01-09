36b5ea983bdddd22cc15ca23d8b560635423df9d
chris <c.herczeg11@hotmail.com>
2020-01-09 14:27:42 -0500

    Added ANSI Green for succeed and ANSI Red for failed in test for core and keyvault

--- sdk/core/core/test/az_test.h
+++ sdk/core/core/test/az_test.h
@@ -3,14 +3,18 @@
 
 #include <assert.h>
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 extern int exit_code;
 
 #define TEST_ASSERT(c) \
   do { \
     if (c) { \
-      printf("  - `%s`: succeeded\n", #c); \
+      printf("  - `%s`: " ANSI_COLOR_GREEN "succeeded" ANSI_COLOR_RESET "\n", #c); \
     } else { \
-      fprintf(stderr, "  - `%s`: failed\n", #c); \
+      fprintf(stderr, "  - `%s`: " ANSI_COLOR_RED "failed" ANSI_COLOR_RESET "\n", #c); \
       assert(false); \
       exit_code = 1; \
     } \
