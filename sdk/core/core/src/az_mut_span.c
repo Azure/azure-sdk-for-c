bfd09d71b71c2a813bbda8e86519f02135c016d6
Victor Vazquez <vhvb1989@gmail.com>
2020-01-14 20:33:20 +0000

    fix previous merge

--- sdk/core/core/src/az_mut_span.c
+++ sdk/core/core/src/az_mut_span.c
@@ -7,6 +7,48 @@
 
 #include <_az_cfg.h>
 
+AZ_NODISCARD az_result
+az_mut_span_move(az_mut_span const buffer, az_span const src, az_mut_span * const out_result) {
+  AZ_CONTRACT_ARG_NOT_NULL(out_result);
+
+  AZ_CONTRACT_ARG_VALID_MUT_SPAN(buffer);
+  AZ_CONTRACT_ARG_VALID_SPAN(src);
+
+  AZ_CONTRACT(buffer.size >= src.size, AZ_ERROR_BUFFER_OVERFLOW);
+
+  if (!az_span_is_empty(src)) {
+    memmove((void *)buffer.begin, (void const *)src.begin, src.size);
+  }
+
+  out_result->begin = buffer.begin;
+  out_result->size = src.size;
+
+  return AZ_OK;
+}
+
+AZ_NODISCARD az_result
+az_mut_span_to_str(az_mut_span const buffer, az_span const src, az_mut_span * const out_result) {
+  AZ_CONTRACT_ARG_NOT_NULL(out_result);
+  AZ_CONTRACT_ARG_VALID_MUT_SPAN(buffer);
+  AZ_CONTRACT_ARG_VALID_SPAN(src);
+
+  if (buffer.size < src.size + 1) {
+    return AZ_ERROR_BUFFER_OVERFLOW;
+  }
+
+  if (!az_span_is_empty(src)) {
+    az_mut_span result = { 0 };
+    AZ_RETURN_IF_FAILED(az_mut_span_move(buffer, src, &result));
+  }
+
+  buffer.begin[src.size] = '\0';
+
+  out_result->begin = buffer.begin;
+  out_result->size = src.size + 1;
+
+  return AZ_OK;
+}
+
 AZ_NODISCARD AZ_INLINE size_t _az_size_min(size_t const a, size_t const b) { return a < b ? a : b; }
 
 AZ_INLINE void _az_uint8_swap(uint8_t * const a, uint8_t * const b) {
@@ -22,45 +64,4 @@ void az_mut_span_swap(az_mut_span const a, az_mut_span const b) {
     --i;
     _az_uint8_swap(pa, pb);
   }
-
-  AZ_NODISCARD az_result az_mut_span_move(
-      az_mut_span const buffer, az_span const src, az_mut_span * const out_result) {
-    AZ_CONTRACT_ARG_NOT_NULL(out_result);
-
-    AZ_CONTRACT_ARG_VALID_MUT_SPAN(buffer);
-    AZ_CONTRACT_ARG_VALID_SPAN(src);
-
-    AZ_CONTRACT(buffer.size >= src.size, AZ_ERROR_BUFFER_OVERFLOW);
-
-    if (!az_span_is_empty(src)) {
-      memmove((void *)buffer.begin, (void const *)src.begin, src.size);
-    }
-
-    out_result->begin = buffer.begin;
-    out_result->size = src.size;
-
-    return AZ_OK;
-  }
-
-  AZ_NODISCARD az_result az_mut_span_to_str(
-      az_mut_span const buffer, az_span const src, az_mut_span * const out_result) {
-    AZ_CONTRACT_ARG_NOT_NULL(out_result);
-    AZ_CONTRACT_ARG_VALID_MUT_SPAN(buffer);
-    AZ_CONTRACT_ARG_VALID_SPAN(src);
-
-    if (buffer.size < src.size + 1) {
-      return AZ_ERROR_BUFFER_OVERFLOW;
-    }
-
-    if (!az_span_is_empty(src)) {
-      az_mut_span result = { 0 };
-      AZ_RETURN_IF_FAILED(az_mut_span_move(buffer, src, &result));
-    }
-
-    buffer.begin[src.size] = '\0';
-
-    out_result->begin = buffer.begin;
-    out_result->size = src.size + 1;
-
-    return AZ_OK;
-  }
+}
