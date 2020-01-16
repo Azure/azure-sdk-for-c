62e1a9bcc022199a247cc4e3fd1fdb2f66cc9f22
Sergey Shandar <sergey.shandar@gmail.com>
2020-01-16 11:31:12 -0800

    "ignore" warning

--- sdk/core/core/test/test_span_builder_replace.c
+++ sdk/core/core/test/test_span_builder_replace.c
@@ -120,11 +120,11 @@ void test_span_builder_replace() {
     az_span_builder builder = az_span_builder_create((az_mut_span)AZ_SPAN_FROM_ARRAY(array));
     az_span initial_state = AZ_STR("1234");
     az_span expected = AZ_STR("1X34AB");
-    az_result ignore = az_span_builder_append(&builder, initial_state);
+    TEST_EXPECT_SUCCESS(az_span_builder_append(&builder, initial_state));
 
     TEST_EXPECT_SUCCESS(az_span_builder_replace(&builder, 1, 2, AZ_STR("X")));
 
-    ignore = az_span_builder_append(&builder, AZ_STR("AB"));
+    TEST_EXPECT_SUCCESS(az_span_builder_append(&builder, AZ_STR("AB")));
 
     az_span const result = az_span_builder_result(&builder);
     TEST_ASSERT(az_span_is_equal(result, expected));
