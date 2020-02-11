0979890120238b73f7db016b87c68c7ed82050d5
Victor Vazquez <vhvb1989@gmail.com>
2020-02-11 18:33:15 +0000

    move facility enum to az_result

--- sdk/core/core/inc/az_result.h
+++ sdk/core/core/inc/az_result.h
@@ -4,13 +4,18 @@
 #ifndef _az_RESULT_H
 #define _az_RESULT_H
 
-#include <az_facility.h>
-
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 
 #include <_az_cfg_prefix.h>
+
+enum {
+  AZ_FACILITY_CORE = 0x1,
+  AZ_FACILITY_JSON = 0x2,
+  AZ_FACILITY_HTTP = 0x3,
+  AZ_FACILITY_STD = 0x7FFF,
+};
 
 enum {
   AZ_ERROR_FLAG = (int32_t)0x80000000,
