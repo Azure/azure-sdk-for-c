125179b075ff72628f89a980de004ac8661664ce
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2020-02-24 17:14:11 -0800

    _az_ => AZ_ (Jeffrey's request)

--- sdk/core/core/src/az_aad.c
+++ sdk/core/core/src/az_aad.c
@@ -121,7 +121,7 @@ AZ_NODISCARD az_result _az_aad_request_token(_az_http_request* ref_request, _az_
   // If we failed to get the token, we return failure/
   az_http_response_status_line status_line = { 0 };
   AZ_RETURN_IF_FAILED(az_http_response_get_status_line(&response, &status_line));
-  if (status_line.status_code != _az_HTTP_STATUS_CODE_OK)
+  if (status_line.status_code != AZ_HTTP_STATUS_CODE_OK)
   {
     return AZ_ERROR_HTTP_AUTHENTICATION_FAILED;
   }
