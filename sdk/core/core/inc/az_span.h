dd79d8a3a9977864d95ec84bd38f6cc0a2517836
Dane Walton <dawalton@microsoft.com>
2020-02-26 16:48:14 -0800

    init each member separately

--- sdk/core/core/inc/az_span.h
+++ sdk/core/core/inc/az_span.h
@@ -57,7 +57,7 @@ AZ_NODISCARD AZ_INLINE int32_t az_span_capacity(az_span span) { return span._int
  * @brief Creates an empty span
  *
  */
-#define AZ_SPAN_NULL (az_span){ 0 }
+#define AZ_SPAN_NULL (az_span){ ._internal.ptr = NULL, ._internal.length = 0, ._internal.capacity = 0 }
 
 // A size of the string literal.
 // Details: to make sure that `S` is a `string literal`, we are appending `""`
