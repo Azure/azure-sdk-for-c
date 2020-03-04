3094430d27c9bbf5461082d5070f3c91969822c6
Daniel Jurek <djurek@microsoft.com>
2020-03-04 11:14:30 -0800

    Doc Generation, Doc Publishing, Release tagging (#400)
    * move api docs under eng/docs/api
    
    add docfx index generation
    
    only use docs for core and storage
    
    incorporate release processes
    
    doc templates, stages, version information
    
    use consistent vm image, valid name for doc generation job when inside a stage
    
    use archetype-c-release
    
    refer to generate_docs.py in api
    
    move docs into normal client build
    
    proper syntax for doc generation template
    
    use dots for path separation in doc artifacts
    
    use name and path to specify library location, copy version.txt up to package staging for tagging
    
    docs -> packages for package artifact staging
    
    release info artifact object creation
    
    convert to JSON one must use .ToString()
    
    CHANGELOG.md, copy items to appropriate output locations
    
    publish docs once at the end of generation
    
    update changelog. make it clear that dev tags going up are just tests
    
    consolidate release artifact and doc generation into a single job
    
    remove obsolete templates
    
    copy package-info.json to docs directory
    
    add storage ci.yml, update core ci.yml to use remote repo at head ref
    
    create bones of CHANGELOG.md for blobs
    
    * client.yml shouldn't build doc artifacts, it is not a UP
    
    * remove unnecessary items from .net build
    
    * use production url for creating index

---
+++ eng/docs/index/Generate-DocIndex.ps1
@@ -0,0 +1,77 @@
+# Generates an index page for cataloging different versions of the Docs
+
+[CmdletBinding()]
+Param (
+    $RepoRoot,
+    $DocGenDir
+)
+
+$ServiceMapping = @{
+    "core"="Core";
+    "storage"="Storage";
+}
+
+Write-Verbose "Name Reccuring paths with variable names"
+$DocFxTool = "${RepoRoot}/docfx/docfx.exe"
+$DocOutDir = "${RepoRoot}/docfx_project"
+
+Write-Verbose "Initializing Default DocFx Site..."
+& "${DocFxTool}" init -q -o "${DocOutDir}"
+
+Write-Verbose "Copying template and configuration..."
+New-Item -Path "${DocOutDir}" -Name "templates" -ItemType "directory"
+Copy-Item "${DocGenDir}/templates/*" -Destination "${DocOutDir}/templates" -Force -Recurse
+Copy-Item "${DocGenDir}/docfx.json" -Destination "${DocOutDir}/" -Force
+
+Write-Verbose "Creating Index using service directory and package names from repo..."
+$ServiceList = Get-ChildItem "$($RepoRoot)/sdk" -Directory -Exclude eng, mgmtcommon, template | Sort-Object
+$YmlPath = "${DocOutDir}/api"
+New-Item -Path $YmlPath -Name "toc.yml" -Force
+
+$TargetServices = $ServiceList | Where-Object { $ServiceMapping.Contains($_.Name) }
+
+Write-Verbose "Creating Index for client packages..."
+foreach ($Dir in $TargetServices)
+{
+    # Generate a new top-level md file for the service
+    New-Item -Path $YmlPath -Name "$($Dir.Name).md" -Force
+
+    # Add service to toc.yml
+    $ServiceName = If ($ServiceMapping.Contains($Dir.Name)) { $ServiceMapping[$Dir.Name] } Else { $Dir.Name }
+    Add-Content -Path "$($YmlPath)/toc.yml" -Value "- name: $($ServiceName)`r`n  href: $($Dir.Name).md"
+
+    $PkgList = Get-ChildItem $Dir.FullName -Directory -Exclude .vs, .vscode
+
+    if (($PkgList | Measure-Object).count -eq 0)
+    {
+        continue
+    }
+    Add-Content -Path "$($YmlPath)/$($Dir.Name).md" -Value "# Client"
+    Add-Content -Path "$($YmlPath)/$($Dir.Name).md" -Value "---"
+    Write-Verbose "Operating on Client Packages for $($Dir.Name)"
+
+    # Generate a new md file for each package in the service
+    foreach ($Pkg in $PkgList)
+    {
+        if (Test-Path "$($pkg.FullName)\src")
+        {
+            $ProjectName = Split-Path "$($pkg.FullName)" -Leaf
+            Add-Content -Path "$($YmlPath)/$($Dir.Name).md" -Value "#### $($ProjectName)"
+        }
+    }
+}
+
+
+Write-Verbose "Creating Site Title and Navigation..."
+New-Item -Path "${DocOutDir}" -Name "toc.yml" -Force
+Add-Content -Path "${DocOutDir}/toc.yml" -Value "- name: Azure SDK for C APIs`r`n  href: api/`r`n  homepage: api/index.md"
+
+Write-Verbose "Copying root markdowns"
+Copy-Item "$($RepoRoot)/README.md" -Destination "${DocOutDir}/api/index.md" -Force
+Copy-Item "$($RepoRoot)/CONTRIBUTING.md" -Destination "${DocOutDir}/api/CONTRIBUTING.md" -Force
+
+Write-Verbose "Building site..."
+& "${DocFxTool}" build "${DocOutDir}/docfx.json"
+
+Copy-Item "${DocGenDir}/assets/logo.svg" -Destination "${DocOutDir}/_site/" -Force
+Copy-Item "${DocGenDir}/assets/toc.yml" -Destination "${DocOutDir}/_site/" -Force
