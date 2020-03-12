c4befcb0c5286e93b96198e1f382ffeb36f96239
Victor Vazquez <victor.vazquez@microsoft.com>
2020-03-12 12:04:19 -0700

    Victor/testing/adding tests for code cov with mocked functions (#442)
    * mocking sample

--- sdk/core/core/inc/az_precondition.h
+++ sdk/core/core/inc/az_precondition.h
@@ -36,9 +36,10 @@ AZ_NODISCARD AZ_INLINE bool az_span_is_valid(az_span span, int32_t min_length, b
   int32_t span_length = az_span_length(span);
   int32_t span_capacity = az_span_capacity(span);
   /* Valid Span is:
-      If the length is greater than or equal to a user defined minimum value AND one of the following:
-        - If null_is_valid is true and the pointer in the span is null, the length and capacity must also
-          be 0. In the case of the pointer not being NULL, two conditions must be met:
+      If the length is greater than or equal to a user defined minimum value AND one of the
+     following:
+        - If null_is_valid is true and the pointer in the span is null, the length and capacity must
+     also be 0. In the case of the pointer not being NULL, two conditions must be met:
         - The length is greater than or equal to zero and the capacity is greater than or equal to
           the length.
   */
--- sdk/core/core/test/cmocka/test_span.c
+++ sdk/core/core/test/cmocka/test_span.c
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
 
+#include <az_result.h>
 #include <az_span.h>
 #include <az_span_private.h>
 
@@ -159,6 +160,44 @@ void az_span_append_u32toa_overflow_fails()
   assert_true(az_span_append_u32toa(buffer, v, &out_span) == AZ_ERROR_INSUFFICIENT_SPAN_CAPACITY);
 }
 
+void az_span_to_lower_test()
+{
+  az_span a = AZ_SPAN_FROM_STR("one");
+  az_span b = AZ_SPAN_FROM_STR("One");
+  az_span c = AZ_SPAN_FROM_STR("ones");
+  az_span d = AZ_SPAN_FROM_STR("ona");
+  assert_true(az_span_is_content_equal_ignoring_case(a, b));
+  assert_false(az_span_is_content_equal_ignoring_case(a, c));
+  assert_false(az_span_is_content_equal_ignoring_case(a, d));
+}
+
+void az_span_to_uint64_return_errors()
+{
+  // sample span
+  az_span sample = AZ_SPAN_FROM_STR("test");
+  uint64_t out = 0;
+
+  assert_true(az_span_to_uint64(sample, &out) == AZ_ERROR_PARSER_UNEXPECTED_CHAR);
+}
+
+void az_span_to_uint32_test()
+{
+  az_span number = AZ_SPAN_FROM_STR("1024");
+  uint32_t value = 0;
+
+  assert_return_code(az_span_to_uint32(number, &value), AZ_OK);
+  assert_int_equal(value, 1024);
+}
+
+void az_span_to_str_test()
+{
+  az_span sample = AZ_SPAN_FROM_STR("hello World!");
+  char str[20];
+
+  assert_return_code(az_span_to_str(str, 20, sample), AZ_OK);
+  assert_string_equal(str, "hello World!");
+}
+
 void test_az_span(void** state)
 {
   (void)state;
@@ -182,7 +221,7 @@ void test_az_span(void** state)
     assert_true(az_span_is_content_equal(b, AZ_SPAN_FROM_STR("")));
   }
 
-  //az_span_append_uint8_NULL_out_span_fails();
+  // az_span_append_uint8_NULL_out_span_fails();
   az_span_append_uint8_overflow_fails();
   az_span_append_uint8_succeeds();
 
@@ -198,4 +237,9 @@ void test_az_span(void** state)
   az_span_append_u32toa_max_uint_succeeds();
   az_span_append_u32toa_NULL_span_fails();
   az_span_append_u32toa_overflow_fails();
+
+  az_span_to_lower_test();
+  az_span_to_uint32_test();
+  az_span_to_str_test();
+  az_span_to_uint64_return_errors();
 }
