56706125a8ad5ca2099e1544d268b9814c84b355
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2020-03-18 03:18:04 -0700

    az_test_definitions.h

--- sdk/core/core/test/cmocka/test_az_http_policy.c
+++ sdk/core/core/test/cmocka/test_az_http_policy.c
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: MIT
 
 #include "az_http_policy_private.h"
+#include "az_test_definitions.h"
 #include <az_credentials.h>
 #include <az_http.h>
 #include <az_http_internal.h>
