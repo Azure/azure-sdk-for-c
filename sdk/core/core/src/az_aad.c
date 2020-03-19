1ffdb52229b00892963a182a98fbd43c0c38d4eb
Victor Vazquez <victor.vazquez@microsoft.com>
2020-03-19 15:55:57 -0700

    Victor/core/json (#482)
    * json refactoring

--- sdk/core/core/src/az_aad.c
+++ sdk/core/core/src/az_aad.c
@@ -134,7 +134,7 @@ AZ_NODISCARD az_result _az_aad_request_token(_az_http_request* request, _az_toke
   AZ_RETURN_IF_FAILED(az_json_parse_by_pointer(body, AZ_SPAN_FROM_STR("/expires_in"), &json_token));
 
   double expires_in_seconds = 0;
-  AZ_RETURN_IF_FAILED(az_json_token_get_number(json_token, &expires_in_seconds));
+  AZ_RETURN_IF_FAILED(az_json_token_get_number(&json_token, &expires_in_seconds));
 
   // We'll assume the token expires 3 minutes prior to its actual expiration.
   int64_t const expires_in_msec
@@ -146,7 +146,7 @@ AZ_NODISCARD az_result _az_aad_request_token(_az_http_request* request, _az_toke
       az_json_parse_by_pointer(body, AZ_SPAN_FROM_STR("/access_token"), &json_token));
 
   az_span access_token = { 0 };
-  AZ_RETURN_IF_FAILED(az_json_token_get_string(json_token, &access_token));
+  AZ_RETURN_IF_FAILED(az_json_token_get_string(&json_token, &access_token));
 
   _az_token new_token = {
     ._internal = {
