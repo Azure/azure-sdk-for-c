a61711bd69ca5daec79c3934e3ceea4d785baffc
John Spaith <jspaith@windows.microsoft.com>
2020-03-25 16:55:36 -0700

    Initial backup of PnP telemetry

---
+++ sdk/iot/pnp/src/az_iot_pnp_client_telemetry.c
@@ -0,0 +1,75 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#include <stdint.h>
+
+#include "az_iot_pnp_client.h"
+#include <az_precondition.h>
+#include <az_precondition_internal.h>
+#include <az_result.h>
+#include <az_span.h>
+
+#include <_az_cfg.h>
+
+static const uint8_t pnp_telemetry_param_separator = '&';
+static const uint8_t pnp_telemetry_param_equals = '=';
+
+static const az_span pnp_telemetry_component_name_param = AZ_SPAN_LITERAL_FROM_STR("$.ifname");
+static const az_span pnp_telemetry_content_type_param = AZ_SPAN_LITERAL_FROM_STR("$.ct");
+static const az_span pnp_telemetry_content_encoding_param = AZ_SPAN_LITERAL_FROM_STR("$.ce");
+
+static az_result az_add_telemetry_property(az_span mqtt_topic, az_span property_name, az_span property_value, az_span* out_mqtt_topic)
+{
+  AZ_RETURN_IF_FAILED(
+    az_span_append_uint8(mqtt_topic, pnp_telemetry_param_separator, &mqtt_topic));
+  AZ_RETURN_IF_FAILED(
+    az_span_append(mqtt_topic, property_name, &mqtt_topic));
+  AZ_RETURN_IF_FAILED(
+    az_span_append_uint8(mqtt_topic, pnp_telemetry_param_equals, &mqtt_topic));
+  AZ_RETURN_IF_FAILED(
+    az_span_append(mqtt_topic, property_value, &mqtt_topic));
+
+  *out_mqtt_topic = mqtt_topic;
+  return AZ_OK;
+}
+
+AZ_NODISCARD az_result az_iot_pnp_client_telemetry_get_publish_topic(
+    az_iot_pnp_client const* client,
+    az_span component_name,
+    az_span mqtt_topic,
+    void* reserved,
+    az_span* out_mqtt_topic)
+{
+  AZ_PRECONDITION_NOT_NULL(client);
+  AZ_PRECONDITION_VALID_SPAN(mqtt_topic, 0, false);
+  AZ_PRECONDITION_NOT_NULL(out_mqtt_topic);
+
+  // TODO: Once approved
+  (void)reserved;
+  // AZ_PRECONDITION_IS_NULL(reserved)
+
+  AZ_RETURN_IF_FAILED(az_iot_hub_client_telemetry_publish_topic_get(
+    &client->_internal.iot_hub_client, NULL, mqtt_topic, &mqtt_topic));
+
+  AZ_RETURN_IF_FAILED(
+    az_add_telemetry_property(mqtt_topic, pnp_telemetry_component_name_param, component_name,
+                             &mqtt_topic));
+
+  if (az_span_ptr(client->_internal.options.content_type) != NULL)
+  {
+      AZ_RETURN_IF_FAILED(
+        az_add_telemetry_property(mqtt_topic, pnp_telemetry_content_type_param,
+        client->_internal.options.content_type, &mqtt_topic));
+  }
+
+  if (az_span_ptr(client->_internal.options.content_encoding) != NULL)
+  {
+      AZ_RETURN_IF_FAILED(
+        az_add_telemetry_property(mqtt_topic, pnp_telemetry_content_encoding_param, client->_internal.options.content_encoding,
+        &mqtt_topic));
+  }
+
+  *out_mqtt_topic = mqtt_topic;
+    
+  return AZ_OK;
+}
