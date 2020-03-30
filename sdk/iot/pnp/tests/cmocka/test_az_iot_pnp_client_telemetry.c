2e2ffa7fe895b3f20dd2feb9e2b13f357b8d162b
John Spaith <jspaith@windows.microsoft.com>
2020-03-30 09:31:23 -0700

    Backing up

--- sdk/iot/pnp/tests/cmocka/test_az_iot_pnp_client_telemetry.c
+++ sdk/iot/pnp/tests/cmocka/test_az_iot_pnp_client_telemetry.c
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 
 #include "test_az_iot_pnp_client.h"
-#include <az_iot_hub_client.h>
+#include <az_iot_pnp_client.h>
 #include <az_span.h>
 
 #include <az_precondition.h>
@@ -21,9 +21,13 @@
 #define TEST_ROOT_INTERFACE_NAME "my_root_interface_name"
 #define TEST_CONTENT_TYPE "my_content_type"
 #define TEST_CONTENT_ENCODING "my_content_encoding"
+#define TEST_COMPONENT_NAME "my_component_name"
 
 static const az_span test_device_id = AZ_SPAN_LITERAL_FROM_STR("my_device");
 static const az_span test_device_hostname = AZ_SPAN_LITERAL_FROM_STR("myiothub.azure-devices.net");
+static const az_span test_root_interface_name = AZ_SPAN_LITERAL_FROM_STR(TEST_ROOT_INTERFACE_NAME);
+static const az_span test_component_name = AZ_SPAN_LITERAL_FROM_STR(TEST_COMPONENT_NAME);
+static const char g_test_correct_pnp_topic_no_options_no_props[] = "devices/my_device/messages/events/";
 
 #ifndef NO_PRECONDITION_CHECKING
 
@@ -41,18 +45,16 @@ static void test_az_iot_pnp_client_telemetry_publish_topic_get_no_options_no_pro
       az_iot_pnp_client_init(
         &client, test_device_hostname, test_device_id, test_root_interface_name, NULL), AZ_OK);
 
-  uint8_t mqtt_topic_buf[_az_COUNTOF(g_test_correct_topic_no_options_no_props) - 1];
+  uint8_t mqtt_topic_buf[_az_COUNTOF(g_test_correct_pnp_topic_no_options_no_props) - 1];
   az_span mqtt_topic = az_span_init(mqtt_topic_buf, 0, _az_COUNTOF(mqtt_topic_buf));
 
   assert_int_equal(
-      az_iot_hub_client_telemetry_publish_topic_get(&client, NULL, mqtt_topic, &mqtt_topic), AZ_OK);
+      az_iot_pnp_client_telemetry_get_publish_topic(&client, test_component_name, mqtt_topic, NULL, &mqtt_topic), AZ_OK);
   assert_memory_equal(
-      g_test_correct_topic_no_options_no_props,
+      g_test_correct_pnp_topic_no_options_no_props,
       (char*)az_span_ptr(mqtt_topic),
-      _az_COUNTOF(g_test_correct_topic_no_options_no_props) - 1);
+      _az_COUNTOF(g_test_correct_pnp_topic_no_options_no_props) - 1);
 }
-
-
 
 int test_iot_pnp_telemetry()
 {
@@ -60,5 +62,11 @@ int test_iot_pnp_telemetry()
   setup_precondition_check_tests();
 #endif // NO_PRECONDITION_CHECKING
 
-  return 0;
+    const struct CMUnitTest tests[] = {
+#ifndef NO_PRECONDITION_CHECKING
+      cmocka_unit_test(test_az_iot_pnp_client_telemetry_publish_topic_get_no_options_no_props_succeed),
+#endif // NO_PRECONDITION_CHECKING
+      
+    };
+    return cmocka_run_group_tests_name("az_iot_pnp_client", tests, NULL, NULL);
 }
