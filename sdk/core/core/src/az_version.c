72d4177261f06fd742a58db2d15fa9f300dc7f6c
Rick Winter <rick.winter@microsoft.com>
2020-04-20 17:13:08 -0700

    Add version header

--- sdk/core/core/src/az_version.c
+++ sdk/core/core/src/az_version.c
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
 
+#include "az_version.h"
+
 int az_core_version_get_major() { return AZ_VERSION_MAJOR; }
 int az_core_version_get_minor() { return AZ_VERSION_MINOR; }
 int az_core_version_get_patch() { return AZ_VERSION_PATCH; }
