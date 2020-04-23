e7738fcca569d5e81dd021d4a35cbcfd5655e4f7
Victor Vazquez <vhvb1989@gmail.com>
2020-04-23 18:14:00 +0000

    Document sample behaviour

--- sdk/samples/keyvault/keyvault/samples/src/keys_client_example.c
+++ sdk/samples/keyvault/keyvault/samples/src/keys_client_example.c
@@ -1,6 +1,45 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
 
+/**
+ * @file keys_client_example.c
+ * @brief
+ * Note:
+ * This sample requires a KeyVault account set up with soft deleting off to be able to create and
+ * delete keys without Purge operation. For Sof-delite keys, purge needs to be done by user after
+ * running this sample and before running it again.
+ *
+ * What this sample does:
+ * 1) Creates a secret-id credential getting tenant-id, client-id and secret-id from environment
+ * (make sure variables AZURE_TENANT_ID, AZURE_CLIENT_ID and AZURE_CLIENT_SECRET are set).
+ *
+ * 2) Creates KeyVault SDK client with default options and KeyVault service URL (needs to be
+ * set on environment variable AZURE_KEYVAULT_URL)
+ *
+ * 3) Creates HTTP Response with stack allocated buffer to hold the responses from Service
+ *
+ * 4) Creates key_options with defaults values and then overrides values for operations and tags
+ * with specific values for a new Key to be created with.
+ *
+ * 5) Calls create Key api using the KeyVault SDK client, http response and key_options.
+ * Also adding a name and type for the new key and a global az_context that can be used for
+ * cancelling request.
+ *
+ * 6) Calls get key api using the same http response
+ *
+ * 7) Parse response payload to get key version. Then copy version from http response to a new stack
+ * allocated buffer.
+ *
+ * 8) Creates a new key version by calling create key with same name
+ *
+ * 9) Gets previous key version
+ *
+ * 10) Deletes the key
+ *
+ * 11) Tries to get key-versions expecting error Not-Found
+ *
+ */
+
 #include <az_context.h>
 #include <az_credentials.h>
 #include <az_http.h>
@@ -33,10 +72,11 @@
 int exit_code = 0;
 
 az_span get_key_version(az_http_response* response);
+az_span const key_name_for_test = AZ_SPAN_LITERAL_FROM_STR("test-new-key");
 
 int main()
 {
-  /************* create credentials as client_id type   ***********/
+  /************* 1) create secret id credentials for request   ***********/
   az_credential_client_secret credential = { 0 };
   // init credential_credentials struc
   az_result const creds_retcode = az_credential_client_secret_init(
@@ -47,7 +87,7 @@ int main()
 
   TEST_FAIL_ON_ERROR(creds_retcode, "Failed to init credential");
 
-  /************ Creates keyvault client    ****************/
+  /************ 2) Creates keyvault client    ****************/
   az_keyvault_keys_client client = { 0 };
   az_keyvault_keys_client_options options = az_keyvault_keys_client_options_default();
 
@@ -58,7 +98,7 @@ int main()
 
   TEST_FAIL_ON_ERROR(operation_result, "Failed to init keys client");
 
-  /******* Create a buffer for response (will be reused for all requests)   *****/
+  /******* 3) Create a buffer for response (will be reused for all requests)   *****/
   uint8_t response_buffer[1024 * 4];
   az_span response_span = AZ_SPAN_FROM_BUFFER(response_buffer);
   az_http_response http_response = { 0 };
@@ -66,7 +106,7 @@ int main()
 
   TEST_FAIL_ON_ERROR(init_http_response_result, "Failed to init http response");
 
-  /******************  CREATE KEY with options******************************/
+  /****************** 4) CREATE KEY with options******************************/
   az_keyvault_create_key_options key_options = { 0 };
 
   // override options values
@@ -77,10 +117,11 @@ int main()
                                   az_pair_from_str("bKey", "bValue"),
                                   AZ_PAIR_NULL };
 
+  // 5) This is the actual call to keyvault service
   az_result const create_result = az_keyvault_keys_key_create(
       &client,
       &az_context_app,
-      AZ_SPAN_FROM_STR("test-new-key"),
+      key_name_for_test,
       az_keyvault_web_key_type_rsa(),
       &key_options,
       &http_response);
@@ -98,46 +139,50 @@ int main()
 
   printf("Key created result: \n%s", response_buffer);
 
-  // Reuse response buffer for create Key by creating a new span from response_buffer
-  az_result const reset1_op = az_http_response_init(&http_response, response_span);
-  TEST_FAIL_ON_ERROR(reset1_op, "Failed to reset http response");
-
-  az_span_fill(http_response._internal.http_response, 'h');
-
-  /******************  GET KEY latest ver ******************************/
+  /****************** 6) GET KEY latest ver ******************************/
   az_result get_key_result = az_keyvault_keys_key_get(
-      &client, &az_context_app, AZ_SPAN_FROM_STR("test-new-key"), AZ_SPAN_NULL, &http_response);
+      &client, &az_context_app, key_name_for_test, AZ_SPAN_NULL, &http_response);
 
   TEST_FAIL_ON_ERROR(get_key_result, "Failed to get key");
 
   printf("\n\n*********************************\nGet Key result: \n%s", response_buffer);
 
-  /****************** get key version from response ****/
-  az_span version = get_key_version(&http_response);
+  /****************** 7) get key version from response ****/
   // version is still at http_response. Let's copy it to a new buffer
-  uint8_t version_buf[40];
-  az_span version_builder = AZ_SPAN_FROM_BUFFER(version_buf);
-  if (az_span_size(version_builder) < az_span_size(version))
+  uint8_t copy_version_buf[40];
+  az_span copy_version_builder = AZ_SPAN_FROM_BUFFER(copy_version_buf);
+  // version span will be pointing to http_response, so it will change as soon as http response is
+  // reused. We just need to get it and then copy it to copy_version_builder to keep it
+  az_span version = get_key_version(&http_response);
   {
-    printf("Failed to append key version");
+    // make sure that the size of new buffer can hold the version on runtime
+    int32_t version_buffer_len = az_span_size(copy_version_builder);
+    int32_t version_len = az_span_size(version);
+    if (version_buffer_len < az_span_size(version))
+    {
+      printf(
+          "Version length is greater (%d) than the buffer used to copy it (%d). Use a larger "
+          "buffer",
+          version_len,
+          version_buffer_len);
+      return exit_code + 1; // Error, terminate proccess with non 0 as error.
+    }
+    else
+    {
+      az_span_copy(copy_version_builder, version);
+      // now let version point to the copy_version_builder and with the right size
+      version = az_span_slice(copy_version_builder, 0, version_len);
+    }
   }
-  else
-  {
-    az_span_copy(version_builder, version);
-    version = az_span_slice(version_builder, 0, az_span_size(version));
-  }
 
-  // Reuse response buffer for delete Key by creating a new span from response_buffer
-  az_result const reset2_op = az_http_response_init(&http_response, response_span);
-  TEST_FAIL_ON_ERROR(reset2_op, "Failed to reset http response");
-
-  az_span_fill(response_span, '.');
-
-  /*********************  Create a new key version (use default options) *************/
+  /*********************  8) Create a new key version (use default options) *************/
+  // We previously created a key and saved its version locally
+  // We are not re-using same http response to create a new version for same already created key
+  // After that, we can get previous version by using the copy of version we got before.
   az_result const create_version_result = az_keyvault_keys_key_create(
       &client,
       &az_context_app,
-      AZ_SPAN_FROM_STR("test-new-key"),
+      key_name_for_test,
       az_keyvault_web_key_type_rsa(),
       NULL,
       &http_response);
@@ -148,36 +193,27 @@ int main()
       "\n\n*********************************\nKey new version created result: \n%s",
       response_buffer);
 
-  // Reuse response buffer for delete Key by creating a new span from response_buffer
-  az_result const reset3_op = az_http_response_init(&http_response, response_span);
-  TEST_FAIL_ON_ERROR(reset3_op, "Failed to reset http response");
-
-  az_span_fill(response_span, '.');
-
-  /******************  GET KEY previous ver ******************************/
+  /****************** 9) GET KEY previous ver ******************************/
+  // Here we use the version we recorded previously as parameter
+  // Getting a key with NULL parameter for version will return latest version
   az_result const get_key_prev_ver_result = az_keyvault_keys_key_get(
-      &client, &az_context_app, AZ_SPAN_FROM_STR("test-new-key"), version, &http_response);
+      &client, &az_context_app, key_name_for_test, version, &http_response);
 
   TEST_FAIL_ON_ERROR(get_key_prev_ver_result, "Failed to get previous version of the key");
 
   printf("\n\n*********************************\nGet Key previous Ver: \n%s", response_buffer);
 
-  /******************  DELETE KEY ******************************/
-  az_result const delete_key_result = az_keyvault_keys_key_delete(
-      &client, &az_context_app, AZ_SPAN_FROM_STR("test-new-key"), &http_response);
+  /****************** 10) DELETE KEY ******************************/
+  az_result const delete_key_result
+      = az_keyvault_keys_key_delete(&client, &az_context_app, key_name_for_test, &http_response);
 
   TEST_FAIL_ON_ERROR(delete_key_result, "Failed to delete key");
 
   printf("\n\n*********************************\nDELETED Key: \n %s", response_buffer);
 
-  // Reuse response buffer for create Key by creating a new span from response_buffer
-  az_result const reset4_op = az_http_response_init(&http_response, response_span);
-  TEST_FAIL_ON_ERROR(reset4_op, "Failed to reset http response");
-  az_span_fill(response_span, '.');
-
-  /******************  GET KEY (should return failed response ) ******************************/
+  /****************** 11) GET KEY (should return failed response ) ******************************/
   az_result get_key_again_result = az_keyvault_keys_key_get(
-      &client, &az_context_app, AZ_SPAN_FROM_STR("test-new-key"), AZ_SPAN_NULL, &http_response);
+      &client, &az_context_app, key_name_for_test, AZ_SPAN_NULL, &http_response);
 
   TEST_FAIL_ON_ERROR(get_key_again_result, "Failed to get key");
 
