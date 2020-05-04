596fb7edcf21884db4c8f57e1031d476a148b329
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2020-05-04 14:02:01 -0700

    [ACR/2] move _az_span_token from az_iot_core.h to az_span_internal.h

--- sdk/core/core/src/az_span.c
+++ sdk/core/core/src/az_span.c
@@ -689,21 +689,19 @@ AZ_NODISCARD az_span _az_span_token(az_span source, az_span delimiter, az_span* 
   {
     return AZ_SPAN_NULL;
   }
+
+  int32_t index = az_span_find(source, delimiter);
+
+  if (index != -1)
+  {
+    *out_remainder = az_span_slice(source, index + az_span_size(delimiter), az_span_size(source));
+
+    return az_span_slice(source, 0, index);
+  }
   else
   {
-    int32_t index = az_span_find(source, delimiter);
+    *out_remainder = AZ_SPAN_NULL;
 
-    if (index != -1)
-    {
-      *out_remainder = az_span_slice(source, index + az_span_size(delimiter), az_span_size(source));
-
-      return az_span_slice(source, 0, index);
-    }
-    else
-    {
-      *out_remainder = AZ_SPAN_NULL;
-
-      return source;
-    }
+    return source;
   }
 }
