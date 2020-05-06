345174d503e29a9b660e193f0475252c6ba0a882
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2020-05-06 05:47:30 -0700

    Fix unit tests

--- sdk/core/core/test/cmocka/test_az_url_encode.c
+++ sdk/core/core/test/cmocka/test_az_url_encode.c
@@ -141,10 +141,10 @@ static void test_url_encode_basic(void** state)
       '*', '*', '*', '*', '*', '*', '*', '*', '*', '*',
     };
 
-    az_span const buffer11 = az_span_slice(AZ_SPAN_FROM_BUFFER(buf20), 0, 11);
+    az_span const buffer12 = az_span_slice(AZ_SPAN_FROM_BUFFER(buf20), 0, 12);
 
     int32_t url_length = 0xFF;
-    assert_true(az_succeeded(_az_span_url_encode(buffer11, AZ_SPAN_FROM_STR("///"), &url_length)));
+    assert_true(az_succeeded(_az_span_url_encode(buffer12, AZ_SPAN_FROM_STR("///"), &url_length)));
 
     assert_int_equal(url_length, sizeof("%2F%2F%2F") - 1);
     assert_true(az_span_is_content_equal(
@@ -157,7 +157,7 @@ static void test_url_encode_basic(void** state)
       '*', '*', '*', '*', '*', '*', '*', '*', '*', '*',
     };
 
-    az_span const buffer10 = az_span_slice(AZ_SPAN_FROM_BUFFER(buf20), 0, 11);
+    az_span const buffer10 = az_span_slice(AZ_SPAN_FROM_BUFFER(buf20), 0, 10);
 
     int32_t url_length = 0xFF;
     assert_true(
@@ -166,7 +166,7 @@ static void test_url_encode_basic(void** state)
 
     assert_int_equal(url_length, 0);
     assert_true(az_span_is_content_equal(
-        AZ_SPAN_FROM_BUFFER(buf20), AZ_SPAN_FROM_STR("AbC%2F%2F**************")));
+        AZ_SPAN_FROM_BUFFER(buf20), AZ_SPAN_FROM_STR("AbC%2F%2F***********")));
   }
   {
     // Could've been enough space to encode 3 characters, but there's only space for two
@@ -185,7 +185,7 @@ static void test_url_encode_basic(void** state)
 
     assert_int_equal(url_length, 0);
     assert_true(az_span_is_content_equal(
-        AZ_SPAN_FROM_BUFFER(buf20), AZ_SPAN_FROM_STR("AbC%2F%2F**************")));
+        AZ_SPAN_FROM_BUFFER(buf20), AZ_SPAN_FROM_STR("AbC%2F%2F***********")));
   }
   {
     // Could've been enough space to encode 3 characters, and there's just enough space.
@@ -194,7 +194,7 @@ static void test_url_encode_basic(void** state)
       '*', '*', '*', '*', '*', '*', '*', '*', '*', '*',
     };
 
-    az_span const buffer12 = az_span_slice(AZ_SPAN_FROM_BUFFER(buf20), 0, 11);
+    az_span const buffer12 = az_span_slice(AZ_SPAN_FROM_BUFFER(buf20), 0, 12);
 
     int32_t url_length = 0xFF;
     assert_true(
@@ -202,7 +202,7 @@ static void test_url_encode_basic(void** state)
 
     assert_int_equal(url_length, sizeof("AbC%2F%2F%2F") - 1);
     assert_true(az_span_is_content_equal(
-        AZ_SPAN_FROM_BUFFER(buf20), AZ_SPAN_FROM_STR("AbC%2F%2F%2F***********")));
+        AZ_SPAN_FROM_BUFFER(buf20), AZ_SPAN_FROM_STR("AbC%2F%2F%2F********")));
   }
 }
 
@@ -266,9 +266,9 @@ static void test_url_encode_preconditions(void** state)
 
       int32_t url_length = 0xFF;
       assert_true(az_succeeded(_az_span_url_encode(out_buffer, in_buffer, &url_length)));
-      assert_int_equal(url_length, sizeof("%2F2F") - 1);
+      assert_int_equal(url_length, sizeof("%2F2F2") - 1);
       assert_true(
-          az_span_is_content_equal(AZ_SPAN_FROM_BUFFER(buf), AZ_SPAN_FROM_STR("%2F2F*******")));
+          az_span_is_content_equal(AZ_SPAN_FROM_BUFFER(buf), AZ_SPAN_FROM_STR("%2F2F2******")));
     }
   }
 #else
@@ -361,29 +361,21 @@ static void test_url_encode_usage(void** state)
 
 static void test_url_encode_full(void** state)
 {
+  // Go through all 256 values.
   (void)state;
 
-  // Go through all 256 values.
-  uint8_t values256[256] = {
-    0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
-    0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F,
-    0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F,
-    0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F,
-    0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F,
-    0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A, 0x5B, 0x5C, 0x5D, 0x5E, 0x5F,
-    0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E, 0x6F,
-    0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7A, 0x7B, 0x7C, 0x7D, 0x7E, 0x7F,
-    0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F,
-    0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97, 0x98, 0x99, 0x9A, 0x9B, 0x9C, 0x9D, 0x9E, 0x9F,
-    0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF,
-    0xB0, 0xB1, 0xB2, 0xB3, 0xB4, 0xB5, 0xB6, 0xB7, 0xB8, 0xB9, 0xBA, 0xBB, 0xBC, 0xBD, 0xBE, 0xBF,
-    0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF,
-    0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0xD5, 0xD6, 0xD7, 0xD8, 0xD9, 0xDA, 0xDB, 0xDC, 0xDD, 0xDE, 0xDF,
-    0xE0, 0xE1, 0xE2, 0xE3, 0xE4, 0xE5, 0xE6, 0xE7, 0xE8, 0xE9, 0xEA, 0xEB, 0xEC, 0xED, 0xEE, 0xEF,
-    0xF0, 0xF1, 0xF2, 0xF3, 0xF4, 0xF5, 0xF6, 0xF7, 0xF8, 0xF9, 0xFA, 0xFB, 0xFC, 0xFD, 0xFE, 0xFF,
-  };
+  uint8_t values256[256] = { 0 };
+  for (int i = 0; i < _az_COUNTOF(buf); ++i)
+  {
+    buf[i] = (uint8_t)i;
+  }
 
   uint8_t buf[256 * 3] = { 0 };
+  for (int i = 0; i < _az_COUNTOF(buf); ++i)
+  {
+    buf[i] = '*';
+  }
+
   az_span const buffer = AZ_SPAN_FROM_BUFFER(buf);
 
   int32_t url_length = 0xFF;
@@ -393,7 +385,8 @@ static void test_url_encode_full(void** state)
   int const nunreserved = sizeof("-_.~"
                                  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
                                  "abcdefghijklmnopqrstuvwxyz"
-                                 "0123456789") - 1;
+                                 "0123456789")
+      - 1;
 
   assert_int_equal(url_length, nunreserved + (256 - nunreserved) * 3);
 
@@ -414,7 +407,15 @@ static void test_url_encode_full(void** state)
                        "%C0%C1%C2%C3%C4%C5%C6%C7%C8%C9%CA%CB%CC%CD%CE%CF"
                        "%D0%D1%D2%D3%D4%D5%D6%D7%D8%D9%DA%DB%DC%DD%DE%DF"
                        "%E0%E1%E2%E3%E4%E5%E6%E7%E8%E9%EA%EB%EC%ED%EE%EF"
-                       "%F0%F1%F2%F3%F4%F5%F6%F7%F8%F9%FA%FB%FC%FD%FE%FF")));
+                       "%F0%F1%F2%F3%F4%F5%F6%F7%F8%F9%FA%FB%FC%FD%FE%FF"
+                       "****************"
+                       "****************"
+                       "****************"
+                       "****************"
+                       "****************"
+                       "****************"
+                       "****************"
+                       "****************")));
 }
 
 int test_az_url_encode()
