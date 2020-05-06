1a78b5cc86823bc8989e770037de515d7da6174c
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2020-05-05 19:12:53 -0700

    ;

--- sdk/core/core/test/cmocka/test_az_url_encode.c
+++ sdk/core/core/test/cmocka/test_az_url_encode.c
@@ -16,7 +16,7 @@
 
 #include <_az_cfg.h>
 
-enable_precondition_check_tests();
+enable_precondition_check_tests()
 
 static az_span url_encoded = AZ_SPAN_LITERAL_FROM_STR(
     "%00%01%02%03%04%05%06%07%08%09%0A%0B%0C%0D%0E%0F%10%11%12%13%14%15%16%17%18%19%1A%1B%1C%1D%1E%"
