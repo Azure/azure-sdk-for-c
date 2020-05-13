79bc6ed8f8857bfcec9adba6a4a79a47e6a33314
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2020-05-13 16:47:39 -0700

    Return non-0 (1) when we find out that the sample was compiled with az_nohttp

--- sdk/storage/blobs/samples/src/blobs_client_example.c
+++ sdk/storage/blobs/samples/src/blobs_client_example.c
@@ -90,7 +90,7 @@ int main()
     printf("Running sample with no_op HTTP implementation.\nRecompile az_core with an HTTP client "
            "implementation like CURL to see sample sending network requests.\n\n"
            "i.e. cmake -DTRANSPORT_CURL=ON ..\n\n");
-    return 0;
+    return 1;
   }
 
   RETURN_IF_FAILED(create_result, "Failed to create blob");
