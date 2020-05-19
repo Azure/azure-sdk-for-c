0700e6f4d6d5d43fdb0f3245c1115067715ece55
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2020-05-19 04:03:01 -0700

    GCC warning, rename

--- sdk/core/az_core/src/az_spinlock.c
+++ sdk/core/az_core/src/az_spinlock.c
@@ -12,10 +12,10 @@
 // The top 2 bits are reserved for writers; the bottom 30 bits are for readers
 enum
 {
-  _az_MASK_WRITER_BIT = (int)0x80000000,
-  _az_MASK_WRITER_WAITING_BIT = 0x40000000,
-  _az_MASK_WRITER_BITS = _az_MASK_WRITER_BIT | _az_MASK_WRITER_WAITING_BIT,
-  _az_MASK_READER_BITS = ~_az_MASK_WRITER_BITS,
+  _az_SPINLOCK_WRITER_BIT = (int)0x80000000,
+  _az_SPINLOCK_WRITER_WAITING_BIT = 0x40000000,
+  _az_SPINLOCK_WRITER_BITS = _az_SPINLOCK_WRITER_BIT | _az_SPINLOCK_WRITER_WAITING_BIT,
+  _az_SPINLOCK_READER_BITS = ~_az_SPINLOCK_WRITER_BITS,
 };
 
 void _az_spinlock_enter_writer(_az_spinlock* lock)
@@ -26,18 +26,18 @@ void _az_spinlock_enter_writer(_az_spinlock* lock)
 
     // If the lock is free (0) or (no readers and a writer is waiting): try to set the writer bit
     // If successful, return
-    if ((state == 0 || state == _az_MASK_WRITER_WAITING_BIT)
+    if ((state == 0 || state == _az_SPINLOCK_WRITER_WAITING_BIT)
         && az_platform_atomic_compare_exchange(
-            &lock->_internal.state, state, (uint32_t)_az_MASK_WRITER_BIT))
+            &lock->_internal.state, state, (uint32_t)_az_SPINLOCK_WRITER_BIT))
     {
       return;
     }
 
     // Otherwise, if no writer is waiting, try to set that a writer is waiting
     // If we fail, we'll spin around and try again
-    if ((state & _az_MASK_WRITER_WAITING_BIT) == 0
+    if ((state & _az_SPINLOCK_WRITER_WAITING_BIT) == 0
         && az_platform_atomic_compare_exchange(
-            &lock->_internal.state, state, state | _az_MASK_WRITER_WAITING_BIT))
+            &lock->_internal.state, state, state | _az_SPINLOCK_WRITER_WAITING_BIT))
     {
       return;
     }
@@ -48,7 +48,7 @@ void _az_spinlock_exit_writer(_az_spinlock* lock)
 {
   // Set the state to 0 (free the lock)
   // But, keep the writer waiting bit (if set) to prevent a reader from getting in.
-  lock->_internal.state = lock->_internal.state & _az_MASK_WRITER_WAITING_BIT;
+  lock->_internal.state = lock->_internal.state & _az_SPINLOCK_WRITER_WAITING_BIT;
 }
 
 void _az_spinlock_enter_reader(_az_spinlock* lock)
@@ -57,7 +57,7 @@ void _az_spinlock_enter_reader(_az_spinlock* lock)
   {
     uint32_t const state = (uint32_t)lock->_internal.state;
 
-    if ((state & _az_MASK_WRITER_BITS) == 0)
+    if ((state & (uint32_t)_az_SPINLOCK_WRITER_BITS) == 0)
     {
       // If no writer, try to add a reader
       uint32_t const incremented_state = state + 1;
@@ -82,7 +82,7 @@ void _az_spinlock_exit_reader(_az_spinlock* lock)
     if (az_platform_atomic_compare_exchange(
             &lock->_internal.state,
             state,
-            ((state & _az_MASK_READER_BITS) - 1) | (state & _az_MASK_WRITER_WAITING_BIT)))
+            ((state & _az_SPINLOCK_READER_BITS) - 1) | (state & _az_SPINLOCK_WRITER_WAITING_BIT)))
     {
       return;
     }
