296fc7cd7c058df2791e7f2ec48033ed6043a8e5
Danny Amirault <dana@microsoft.com>
2020-05-21 14:44:49 -0700

    Refactor version logic

--- eng/scripts/Update-SdkVersion.ps1
+++ eng/scripts/Update-SdkVersion.ps1
@@ -6,9 +6,6 @@ Bumps up sdk version after release
 This script bumps up the sdk version found in az_version.h following conventions defined at https://github.com/Azure/azure-sdk/blob/master/docs/policies/releases.md#incrementing-after-release-c
 We use the version number defined in AZ_SDK_VERSION_STRING, and then overwrite other #defines
 
-.PARAMETER RepoRoot
-The Root of the repo
-
 .PARAMETER NewVersionString
 Use this to overide version incement logic and set a version specified by this parameter
 
@@ -25,11 +22,11 @@ Update-SdkVersion.ps1 -NewVersionString 2.0.5
 [CmdletBinding()]
 Param (
     [ValidateNotNullOrEmpty()]
-    [string] $RepoRoot = "${PSScriptRoot}/../..",
     [string] $NewVersionString
 )
 
 . ${PSScriptRoot}\..\common\scripts\SemVer.ps1
+. ${PSScriptRoot}\SdkVersion-Common.ps1
 
 # Updated Version in version file and changelog using computed or set NewVersionString
 function Update-Version([AzureEngSemanticVersion]$SemVer, $Unreleased=$True, $ReplaceVersion=$False)
@@ -47,37 +44,26 @@ function Update-Version([AzureEngSemanticVersion]$SemVer, $Unreleased=$True, $Re
         $PrereleaseDefine = "#undef AZ_SDK_VERSION_PRERELEASE"
     }
 
-    (Get-Content -Path $PackageVersionPath -Raw) `
+    (Get-Content -Path $SdkVersionPath -Raw) `
      -replace $VersionStringRegEx, "`${1}`"$($SemVer)`"" `
      -replace $VersionMajorRegEx,  "`${1}$($SemVer.Major)" `
      -replace $VersionMinorRegEx,  "`${1}$($SemVer.Minor)" `
      -replace $VersionPatchRegEx,  "`${1}$($SemVer.Patch)" `
      -replace $VersionPrereleaseRegEx,  $PrereleaseDefine |
-     Set-Content -Path $PackageVersionPath -NoNewline
+     Set-Content -Path $SdkVersionPath -NoNewline
 
     # Increment Version in ChangeLog file
     & "${PSScriptRoot}/../common/Update-Change-Log.ps1" -Version $SemVer.ToString() -ChangeLogPath $ChangelogPath -Unreleased $Unreleased -ReplaceVersion $ReplaceVersion
 }
 
-# Obtain Current Package Version
-# We use the version number defined in AZ_SDK_VERSION_STRING, and then overwrite other #defines
 
-$PackageVersionPath = Join-Path $RepoRoot "sdk\core\az_core\inc\az_version.h"
-$ChangelogPath = Join-Path $RepoRoot "sdk\core\az_core\CHANGELOG.md"
-$PackageVersionFile = Get-Content -Path $PackageVersionPath -Raw
-$VersionStringRegEx = '(#define AZ_SDK_VERSION_STRING )"(([0-9]+)\.([0-9]+)\.([0-9]+)(\-[^\"\-]+)?)"';
-$VersionMajorRegEx = '(#define AZ_SDK_VERSION_MAJOR )([0-9]+)';
-$VersionMinorRegEx = '(#define AZ_SDK_VERSION_MINOR )([0-9]+)';
-$VersionPatchRegEx = '(#define AZ_SDK_VERSION_PATCH )([0-9]+)';
-$VersionPrereleaseRegEx = '(#(un)?def(ine)? AZ_SDK_VERSION_PRERELEASE) ?("[a-z0-9.]+")?';
-
-$match = $PackageVersionFile -match $VersionStringRegEx
-$PackageVersion = $Matches[2]
+$match = $SdkVersionFile -match $VersionStringRegEx
+$SdkVersion = $Matches[2]
 
 if ([System.String]::IsNullOrEmpty($NewVersionString))
 {
-    $SemVer = [AzureEngSemanticVersion]::new($PackageVersion)
-    Write-Output "Current Version: ${PackageVersion}"
+    $SemVer = [AzureEngSemanticVersion]::new($SdkVersion)
+    Write-Output "Current Version: ${SdkVersion}"
 
     $SemVer.IncrementAndSetToPrerelease()
     Update-Version -SemVer $SemVer
