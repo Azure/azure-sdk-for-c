9902bcc3bd6c9a43fb45b16427df53f92b2bfdc4
ewertons <ewertons@microsoft.com>
2020-06-10 13:13:55 -0700

    Update sdk/iot/hub/samples/esp8266nodemcu/iot_configs.h

--- sdk/iot/hub/samples/esp8266nodemcu/iot_configs.h
+++ sdk/iot/hub/samples/esp8266nodemcu/iot_configs.h
@@ -3,7 +3,7 @@
 #define IOT_CONFIG_WIFI_PASSWORD        "PWD"
 
 // Azure IoT
-#define IOT_CONFIG_IOTHUB_FQDN          "[your host name].azure-devices.net"
+#define IOT_CONFIG_IOTHUB_FQDN          "[your Azure IoT host name].azure-devices.net"
 #define IOT_CONFIG_DEVICE_ID            "Device ID"
 #define IOT_CONFIG_DEVICE_KEY           "Device Key"
 
