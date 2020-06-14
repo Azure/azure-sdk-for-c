5327ed1bdaeb95316db6e699d4a3d3271c34b12e
Dane Walton <dawalton@microsoft.com>
2020-06-13 20:07:50 -0700

    update Arduino script

--- sdk/samples/iot/hub/esp8266nodemcu/generate_arduino_zip_library.sh
+++ sdk/samples/iot/hub/esp8266nodemcu/generate_arduino_zip_library.sh
@@ -3,15 +3,14 @@ command -v zip >/dev/null 2>&1 || { echo >&2 "Please install zip."; exit 1; }
 git clone https://github.com/Azure/azure-sdk-for-c sdkrepo
 mkdir azure-sdk-for-c
 
-cp -r sdkrepo/sdk/core/az_core/inc/* azure-sdk-for-c/
-cp -r sdkrepo/sdk/core/az_core/internal/* azure-sdk-for-c/
-cp -r sdkrepo/sdk/core/az_core/src/* azure-sdk-for-c/
-cp -r sdkrepo/sdk/platform/noplatform/src/* azure-sdk-for-c/
+mkdir -p azure-sdk-for-c/azure/core
+cp -r sdkrepo/sdk/inc/azure/core/* azure-sdk-for-c/azure/core
+mkdir -p azure-sdk-for-c/azure/iot/
+cp -r sdkrepo/sdk/inc/azure/iot/* azure-sdk-for-c/azure/iot
 
-cp -r sdkrepo/sdk/iot/hub/inc/* azure-sdk-for-c/
-cp -r sdkrepo/sdk/iot/hub/src/* azure-sdk-for-c/
-cp -r sdkrepo/sdk/iot/common/inc/* azure-sdk-for-c/
-cp -r sdkrepo/sdk/iot/common/src/* azure-sdk-for-c/
+cp -r sdkrepo/sdk/src/azure/core/* azure-sdk-for-c/
+cp -r sdkrepo/sdk/src/azure/iot/* azure-sdk-for-c/
+cp sdkrepo/sdk/src/azure/platform/az_noplatform.c azure-sdk-for-c/
 
 zip -r9 azure-sdk-for-c azure-sdk-for-c/
 
