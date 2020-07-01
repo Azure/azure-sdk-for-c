8c45ef76a8b22f0423639b6ac3ec3c6bfaca310d
Rick Winter <rick.winter@microsoft.com>
2020-07-01 09:44:47 -0700

    Switch to ports folder for the platform and http_adapter

--- sdk/ports/win32/az_platform_impl.h
+++ sdk/ports/win32/az_platform_impl.h
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
 
-#include <azure/core/az_platform.h>
-
 // Two macros below are not used in the code below, it is windows.h that consumes them.
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
@@ -10,11 +8,11 @@
 
 #include <azure/core/_az_cfg.h>
 
-AZ_NODISCARD int64_t az_platform_clock_msec() { return GetTickCount64(); }
+inline AZ_NODISCARD int64_t az_platform_clock_msec() { return GetTickCount64(); }
 
-void az_platform_sleep_msec(int32_t milliseconds) { Sleep(milliseconds); }
+inline void az_platform_sleep_msec(int32_t milliseconds) { Sleep(milliseconds); }
 
-AZ_NODISCARD bool az_platform_atomic_compare_exchange(
+inline AZ_NODISCARD bool az_platform_atomic_compare_exchange(
     uintptr_t volatile* obj,
     uintptr_t expected,
     uintptr_t desired)
