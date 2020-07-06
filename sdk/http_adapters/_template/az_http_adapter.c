c79fa5b050f978d89d0f0bd10a6f13d4742cdb70
Rick Winter <rick.winter@microsoft.com>
2020-07-06 15:30:00 -0700

    Add ports folder for building platform

---
+++ sdk/http_adapters/_template/az_http_adapter.c
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#include <azure/core/az_http_transport.h>
+
+#include <azure/core/_az_cfg.h>
+
+/**
+ * @brief Provides no HTTP support.
+ *
+ * @param request An internal HTTP builder with data to build and send HTTP request.
+ * @param ref_response A pre-allocated buffer where the HTTP response will be written.
+ * @retval An #az_result value indicating the result of the operation:
+ *         - #AZ_OK if successful
+ */
+AZ_NODISCARD az_result
+az_http_client_send_request(_az_http_request const* request, az_http_response* ref_response)
+{
+  (void)request;
+  (void)ref_response;
+  return AZ_ERROR_NOT_IMPLEMENTED;
+}
