063cc92f42da729420167106885a8387bdef77b8
Rick Winter <rick.winter@microsoft.com>
2020-07-08 10:26:31 -0700

    Support building for specific platform implementations. The build will fail when a platform is required but not specified

--- sdk/inc/azure/core/az_platform.h
+++ sdk/inc/azure/core/az_platform.h
@@ -11,41 +11,16 @@
 
 #include <azure/core/_az_cfg_prefix.h>
 
-/**
- * @brief Gets the platform clock in milliseconds.
- * @remark The moment of time where clock starts is undefined, but if this function is getting
- * called twice with one second interval, the difference between the values returned should be equal
- * to 1000.
- * @return Platform clock in milliseconds.
- */
-AZ_NODISCARD int64_t az_platform_clock_msec();
+#if __has_include("az_platform_impl.h")
+    #include <az_platform_impl.h>
+#else
+    // A platform implementation is required when including az_platform.h.
+    // The Azure SDK for Embedded C provides default implementations for Win32, Linux and Mac.
+    // Developers can provide other platform implementations as well.
+    //  https://github.com/Azure/azure-sdk-for-c#cmake-options
+    #error "No platform implementation provided, see documentation for additional details.  https://github.com/Azure/azure-sdk-for-c#cmake-options"
 
-/**
- * @brief Tells the platform to sleep for a given number of milliseconds.
- * @param milliseconds Number of milliseconds to sleep.
- * @remarks The behavior is undefined when \p milliseconds is a non-positive value (0 or less than
- * 0).
- */
-void az_platform_sleep_msec(int32_t milliseconds);
-
-/**
- * @brief Conditionally exchanges values of two pointers in a
- * thread-safe manner.
- *
- * @param obj A pointer to a pointer that needs to be changed.
- * @param expected An expected value of a value that \p obj is pointing to, prior to exchange.
- * @param desired A value to assign to the value that is pointed by \p obj, if its value equals to
- * \p expected.
- *
- * @return `true` if previous value of \p obj matches the \p expected.
- *
- * @remarks If `*obj` equals to \p expected, its value is being overwritten with \p desired, and no
- * operation otherwise. Returns `true` if the value was overwritten, `false` otherwise.
- */
-AZ_NODISCARD bool az_platform_atomic_compare_exchange(
-    uintptr_t volatile* obj,
-    uintptr_t expected,
-    uintptr_t desired);
+#endif //__has_include
 
 #include <azure/core/_az_cfg_suffix.h>
 
