a46e64d7e42fe0543b6c293a873289ccb0f0b753
Ahson Khan <ahkha@microsoft.com>
2020-07-31 17:48:19 -0700

    Testing: Update paths to changelog in eng scripts.

--- eng/scripts/SdkVersion-Common.ps1
+++ eng/scripts/SdkVersion-Common.ps1
@@ -1,7 +1,7 @@
 # NOTE: Update-SdkVersion relies on these variables
 $RepoRoot = "${PSScriptRoot}/../.."
 $SdkVersionPath = Join-Path $RepoRoot "sdk\inc\azure\core\az_version.h"
-$ChangelogPath = Join-Path $RepoRoot "sdk\docs\core\CHANGELOG.md"
+$ChangelogPath = Join-Path $RepoRoot "CHANGELOG.md"
 $SdkVersionFile = Get-Content -Path $SdkVersionPath -Raw
 $VersionStringRegEx = '(#define AZ_SDK_VERSION_STRING )"(([0-9]+)\.([0-9]+)\.([0-9]+)(\-[^\"\-]+)?)"';
 $VersionMajorRegEx = '(#define AZ_SDK_VERSION_MAJOR )([0-9]+)';
