c0db66327462bc3b42162960d664ee11b3be4c2e
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2020-08-04 22:31:34 -0700

    Context const key&value signature fix

--- sdk/inc/azure/core/az_context.h
+++ sdk/inc/azure/core/az_context.h
@@ -132,7 +132,7 @@ AZ_NODISCARD AZ_INLINE bool az_context_has_expired(az_context const* context, in
  * @return  #AZ_OK if the key is found
  *          #AZ_ERROR_ITEM_NOT_FOUND if no nodes are found with the specified key.
  */
-AZ_NODISCARD az_result az_context_get_value(az_context const* context, void* key, void** out_value);
+AZ_NODISCARD az_result az_context_get_value(az_context const* context, void const* key, void const** out_value);
 
 #include <azure/core/_az_cfg_suffix.h>
 
