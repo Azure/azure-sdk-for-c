a5184b87b657f9780f647006d964dbb65ba5c749
Ahson Khan <ahkha@microsoft.com>
2020-08-06 14:58:51 -0700

    Treat linker warnings as errors on MSVC.

--- cmake-modules/AddTestCMocka.cmake
+++ cmake-modules/AddTestCMocka.cmake
@@ -101,14 +101,6 @@ function(ADD_CMOCKA_TEST _TARGET_NAME)
         )
     endif()
 
-    # Workaround for linker warning LNK4098: defaultlib 'LIBCMTD' conflicts with use of other libs
-    if (MSVC)
-     set_target_properties(${_TARGET_NAME}
-            PROPERTIES LINK_FLAGS
-            "/NODEFAULTLIB:libcmtd.lib"
-        )
-    endif()
-
     target_include_directories(${_TARGET_NAME} PRIVATE ${CMOCKA_INCLUDE_DIR})
     
     if (DEFINED _add_cmocka_test_PRIVATE_ACCESS)
