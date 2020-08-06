36d3ca0e619285c719dd15f00542276c79028c23
Dane Walton <dane.walton@microsoft.com>
2020-08-05 17:32:56 -0700

    update get_request_id

--- sdk/samples/iot/sample_pnp_component_mqtt.c
+++ sdk/samples/iot/sample_pnp_component_mqtt.c
@@ -7,7 +7,7 @@
 
 #include <azure/core/az_span.h>
 
-static int32_t request_id_int;
+static uint32_t request_id_int;
 static char request_id_buf[8];
 
 // Create request id span which increments request id integer each call. Capable of holding 8 digit
@@ -16,8 +16,10 @@ az_span get_request_id(void)
 {
   az_span remainder;
   az_span out_span = az_span_init((uint8_t*)request_id_buf, sizeof(request_id_buf));
-  az_result result = az_span_i32toa(out_span, request_id_int++, &remainder);
-  (void)remainder;
+
+  // Note that if left to run for a long time, this will overflow and reset back to 0.
+  az_result result = az_span_u32toa(out_span, request_id_int++, &remainder);
   (void)result;
-  return out_span;
+
+  return az_span_slice(out_span, 0, (int32_t)(az_span_ptr(remainder) - az_span_ptr(out_span)));
 }
