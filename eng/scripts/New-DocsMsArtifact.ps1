9791ac5bd7230937e973be178023d4309904fac0
Daniel Jurek <djurek@microsoft.com>
2020-08-31 14:32:44 -0700

    use --quiet to avoid winston error

--- eng/scripts/New-DocsMsArtifact.ps1
+++ eng/scripts/New-DocsMsArtifact.ps1
@@ -3,4 +3,4 @@ npm i -g moxygen
 # Moxygen will fail if docs/docs.ms does not exist
 mkdir -p docs/docs.ms
 
-moxygen --anchors --output docs/docs.ms/api-docs.md docs/xml
+moxygen --quiet --anchors --output docs/docs.ms/api-docs.md docs/xml
