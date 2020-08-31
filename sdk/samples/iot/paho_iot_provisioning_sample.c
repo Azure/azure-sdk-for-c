c7ba9cb0c199ce26ba11a255ee4cce4e1b0a360b
Mollie Munoz <momuno@microsoft.com>
2020-08-31 13:52:49 -0700

    comment fixes

--- sdk/samples/iot/paho_iot_provisioning_sample.c
+++ sdk/samples/iot/paho_iot_provisioning_sample.c
@@ -50,7 +50,7 @@ static void parse_device_registration_status_message(
     az_iot_provisioning_client_operation_status* out_operation_status);
 static void handle_device_registration_status_message(
     az_iot_provisioning_client_register_response const* register_response,
-    az_iot_provisioning_client_operation_status const* operation_status,
+    az_iot_provisioning_client_operation_status operation_status,
     bool* ref_is_operation_complete);
 static void send_operation_query_message(
     az_iot_provisioning_client_register_response const* response);
@@ -274,7 +274,7 @@ static void receive_device_registration_status_message(void)
     IOT_SAMPLE_LOG_SUCCESS("Client parsed registration status message.");
 
     handle_device_registration_status_message(
-        &register_response, &operation_status, &is_operation_complete);
+        &register_response, operation_status, &is_operation_complete);
 
     MQTTClient_freeMessage(&message);
     MQTTClient_free(topic);
@@ -332,10 +332,10 @@ static void parse_device_registration_status_message(
 
 static void handle_device_registration_status_message(
     az_iot_provisioning_client_register_response const* register_response,
-    az_iot_provisioning_client_operation_status const* operation_status,
+    az_iot_provisioning_client_operation_status operation_status,
     bool* ref_is_operation_complete)
 {
-  *ref_is_operation_complete = az_iot_provisioning_client_operation_complete(*operation_status);
+  *ref_is_operation_complete = az_iot_provisioning_client_operation_complete(operation_status);
 
   // If operation is not complete, send query. On return, will loop to receive new operation
   // message.
@@ -348,7 +348,7 @@ static void handle_device_registration_status_message(
   }
   else // Operation is complete.
   {
-    if (AZ_IOT_PROVISIONING_STATUS_ASSIGNED == *operation_status) // Successful assignment
+    if (operation_status == AZ_IOT_PROVISIONING_STATUS_ASSIGNED) // Successful assignment
     {
       IOT_SAMPLE_LOG_SUCCESS("Device provisioned:");
       IOT_SAMPLE_LOG_AZ_SPAN(
--- sdk/samples/iot/paho_iot_provisioning_sas_sample.c
+++ sdk/samples/iot/paho_iot_provisioning_sas_sample.c
@@ -55,7 +55,7 @@ static void parse_device_registration_status_message(
     az_iot_provisioning_client_operation_status* out_operation_status);
 static void handle_device_registration_status_message(
     az_iot_provisioning_client_register_response const* register_response,
-    az_iot_provisioning_client_operation_status const* operation_status,
+    az_iot_provisioning_client_operation_status operation_status,
     bool* ref_is_operation_complete);
 static void send_operation_query_message(
     az_iot_provisioning_client_register_response const* response);
@@ -283,7 +283,7 @@ static void receive_device_registration_status_message(void)
     IOT_SAMPLE_LOG_SUCCESS("Client parsed registration status message.");
 
     handle_device_registration_status_message(
-        &register_response, &operation_status, &is_operation_complete);
+        &register_response, operation_status, &is_operation_complete);
 
     MQTTClient_freeMessage(&message);
     MQTTClient_free(topic);
@@ -341,10 +341,10 @@ static void parse_device_registration_status_message(
 
 static void handle_device_registration_status_message(
     az_iot_provisioning_client_register_response const* register_response,
-    az_iot_provisioning_client_operation_status const* operation_status,
+    az_iot_provisioning_client_operation_status operation_status,
     bool* ref_is_operation_complete)
 {
-  *ref_is_operation_complete = az_iot_provisioning_client_operation_complete(*operation_status);
+  *ref_is_operation_complete = az_iot_provisioning_client_operation_complete(operation_status);
 
   // If operation is not complete, send query. On return, will loop to receive new operation
   // message.
@@ -357,7 +357,7 @@ static void handle_device_registration_status_message(
   }
   else // Operation is complete.
   {
-    if (AZ_IOT_PROVISIONING_STATUS_ASSIGNED == *operation_status) // Successful assignment
+    if (operation_status == AZ_IOT_PROVISIONING_STATUS_ASSIGNED) // Successful assignment
     {
       IOT_SAMPLE_LOG_SUCCESS("Device provisioned:");
       IOT_SAMPLE_LOG_AZ_SPAN(
