8a40e1cd8b51303b9511030598020d166b760b89
Daniel Jurek <djurek@microsoft.com>
2020-09-04 10:54:29 -0700

    Use a module for producting map files

---
+++ cmake-modules/create_map_file.cmake
@@ -0,0 +1,16 @@
+# Instructs linker to generate map files and optimize build for minimal size
+# Requires CMake version >= 3.13 to use add_link_options
+
+function(create_map_file MAP_FILE_NAME)
+    if (${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.13")
+        if(MSVC)
+            add_link_options(/MAP)
+        elseif(CMAKE_C_COMPILER_ID MATCHES "Clang")
+            add_link_options(-Wl,-map,${MAP_FILE_NAME})
+            add_link_options(-Os)
+        else()
+            add_link_options(-Xlinker -Map=${MAP_FILE_NAME})
+            add_link_options(-Os)
+        endif()
+    endif()
+endfunction()
