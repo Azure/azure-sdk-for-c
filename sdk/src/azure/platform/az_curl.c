c0baa365b25928f817785809bb912ee47de86986
Ahson Khan <ahkha@microsoft.com>
2020-09-14 23:35:36 -0700

    [clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling] memcpy and memset - disable

--- sdk/src/azure/platform/az_curl.c
+++ sdk/src/azure/platform/az_curl.c
@@ -391,6 +391,7 @@ static int32_t _az_http_client_curl_upload_read_callback(
   // copy a next chunk of data
   int32_t size_of_copy = (userdata_length < dst_buffer_size) ? userdata_length : dst_buffer_size;
 
+  // NOLINTNEXTLINE(clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling)
   memcpy(dst, az_span_ptr(*upload_content), (size_t)size_of_copy);
 
   // Update the userdata span. If we already copied all content, slice will set upload_content with
@@ -501,6 +502,7 @@ _az_http_client_curl_setup_url(CURL* ref_curl, az_http_request const* request)
   }
 
   // free used buffer before anything else
+  // NOLINTNEXTLINE(clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling)
   memset(az_span_ptr(writable_buffer), 0, (size_t)az_span_size(writable_buffer));
   _az_span_free(&writable_buffer);
 
