93fcf8e89ca69b2a0b472154fa1203adf4694203
Victor Vazquez <vhvb1989@gmail.com>
2020-09-24 21:41:49 +0000

    update nane for generated docs

--- eng/scripts/New-DocsMsArtifact.ps1
+++ eng/scripts/New-DocsMsArtifact.ps1
@@ -1,6 +1,6 @@
 npm i -g moxygen
 
-# Moxygen will fail if doc/docs/docs.ms does not exist
-mkdir -p doc/docs/docs.ms
+# Moxygen will fail if doc/generated_doxygen_docs/docs.ms does not exist
+mkdir -p doc/generated_doxygen_docs/docs.ms
 
-moxygen --anchors --output doc/docs/docs.ms/api-docs.md doc/docs/xml
+moxygen --anchors --output doc/generated_doxygen_docs/docs.ms/api-docs.md doc/generated_doxygen_docs/xml
