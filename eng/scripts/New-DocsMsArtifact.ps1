06db02f5e30c85a0e32e7232e17ff34ddeba868b
Victor Vazquez <vhvb1989@gmail.com>
2020-09-24 18:45:38 +0000

    update eng scripts for artifects

--- eng/scripts/New-DocsMsArtifact.ps1
+++ eng/scripts/New-DocsMsArtifact.ps1
@@ -1,6 +1,6 @@
 npm i -g moxygen
 
-# Moxygen will fail if docs/docs.ms does not exist
-mkdir -p docs/docs.ms
+# Moxygen will fail if doc/docs/docs.ms does not exist
+mkdir -p doc/docs/docs.ms
 
-moxygen --anchors --output docs/docs.ms/api-docs.md docs/xml
+moxygen --anchors --output doc/docs/docs.ms/api-docs.md doc/docs/xml
