6c34dd48a5b2ff3e81d402321ced55af15854501
Dane Walton <dane.walton@microsoft.com>
2020-09-30 13:21:58 -0700

    update name of component sample (#1385)

--- sdk/samples/iot/iot_sample_common.c
+++ sdk/samples/iot/iot_sample_common.c
@@ -149,10 +149,10 @@ void iot_sample_read_environment_variables(
     {
       case PAHO_IOT_HUB_C2D_SAMPLE:
       case PAHO_IOT_HUB_METHODS_SAMPLE:
-      case PAHO_IOT_HUB_PNP_COMPONENT_SAMPLE:
       case PAHO_IOT_HUB_PNP_SAMPLE:
       case PAHO_IOT_HUB_TELEMETRY_SAMPLE:
       case PAHO_IOT_HUB_TWIN_SAMPLE:
+      case PAHO_IOT_PNP_COMPONENT_SAMPLE:
         out_env_vars->hub_device_id = AZ_SPAN_FROM_BUFFER(iot_sample_hub_device_id_buffer);
         read_configuration_entry(
             IOT_SAMPLE_ENV_HUB_DEVICE_ID,
