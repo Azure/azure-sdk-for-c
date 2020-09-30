70555648dad78c2f2c96e78566bd21589587ba3d
jbobotek <jeremy.bobotek@gmail.com>
2020-09-30 03:11:35 -0700

    Move generate certificate step to eng/scripts/Generate-X509-Certificate.ps1

---
+++ eng/scripts/Generate-X509-Certificate.ps1
@@ -0,0 +1,42 @@
+#!/usr/bin/env pwsh
+
+#Requires -Version 6.0
+#Requires -PSEdition Core
+#Requires -Modules @{ModuleName='Az.DeviceProvisioningServices'; ModuleVersion='0.7.2'}
+param(
+[Parameter(Mandatory = $true)] [string]$SOURCES_DIR,
+[Parameter(Mandatory = $true)] [string]$AZURE_RESOURCEGROUP_NAME,
+[Parameter(Mandatory = $true)] [string]$IOTHUB_NAME,
+[Parameter(Mandatory = $true)] [string]$DEVICE_ID,
+[Parameter(Mandatory = $true)] [string]$DPS_NAME
+
+)
+
+# Generate certificate 
+$orig_loc = Get-Location
+cd $SOURCES_DIR\sdk\samples\iot\
+openssl ecparam -out device_ec_key.pem -name prime256v1 -genkey
+openssl req -new -days 365 -nodes -x509 -key device_ec_key.pem -out device_ec_cert.pem -config x509_config.cfg -subj "/CN=$DEVICE_ID"
+dir
+
+Get-Content device_ec_cert.pem, device_ec_key.pem | Set-Content device_cert_store.pem
+openssl x509 -noout -fingerprint -in device_ec_cert.pem | % {$_.replace(":", "")} | % {$_.replace("SHA1 Fingerprint=", "")} | Tee-Object fingerprint.txt
+$fingerprint = Get-Content -Path .\fingerprint.txt
+
+# Pass fingerprint to IoTHub 
+Add-AzIotHubDevice `
+-ResourceGroupName $AZURE_RESOURCEGROUP_NAME `
+-IotHubName $IOTHUB_NAME `
+-DeviceId $DEVICE_ID `
+-AuthMethod "x509_thumbprint" `
+-PrimaryThumbprint $fingerprint `
+-SecondaryThumbprint $fingerprint
+
+# Download Baltimore Cert
+curl https://cacerts.digicert.com/BaltimoreCyberTrustRoot.crt.pem > $SOURCES_DIR\sdk\samples\iot\BaltimoreCyberTrustRoot.crt.pem
+
+# Link IoTHub to DPS service
+$hubConnectionString=Get-AzIotHubConnectionString -ResourceGroupName $AZURE_RESOURCEGROUP_NAME -Name $IOTHUB_NAME -KeyName "iothubowner"
+Add-AzIoTDeviceProvisioningServiceLinkedHub -ResourceGroupName $AZURE_RESOURCEGROUP_NAME -Name $DPS_NAME -IotHubConnectionString $hubConnectionString.PrimaryConnectionString --IotHubLocation eastus
+
+Set-Location $orig_loc
