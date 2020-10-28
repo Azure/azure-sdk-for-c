4a6c28d91d8f34cb0eba81a4bd8a87b3869ace62
jbobotek <jeremy.bobotek@gmail.com>
2020-10-28 01:07:53 -0700

    Import-Module?

--- sdk/samples/iot/templates/iothub/test-resources-post.ps1
+++ sdk/samples/iot/templates/iothub/test-resources-post.ps1
@@ -7,6 +7,7 @@ param(
 # setup
 #Uninstall-AzureRm -Force
 Install-Module -Name Az.DeviceProvisioningServices -Force
+Import-Module Az.DeviceProvisioningServices
 $orig_loc = Get-Location
 Write-Host $orig_loc
 #Write-Host "##vso[task.setvariable variable=VCPKG_DEFAULT_TRIPLET]:x64-windows-static"
