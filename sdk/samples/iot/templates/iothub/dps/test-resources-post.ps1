0281a5bf45379f38891c62c667a5d186a38c5ac6
jbobotek <jeremy.bobotek@gmail.com>
2020-11-04 12:14:02 -0800

    import both IoTHub modules

--- sdk/samples/iot/templates/iothub/dps/test-resources-post.ps1
+++ sdk/samples/iot/templates/iothub/dps/test-resources-post.ps1
@@ -14,8 +14,7 @@ if (!$IsWindows) { $module_location_prefix = "$HOME\.local\share\powershell\Modu
 if ($IsWindows) { $module_location_prefix = "$HOME\Documents\PowerShell\Modules" }
 
 try {
-Import-Module $module_location_prefix\Az.IotHub -Cmdlet Add-AzIotHubDevice -Force 
-Import-Module $module_location_prefix\Az.IotHub -Cmdlet Get-AzIotHubDeviceConnectionString -Force
+Import-Module $module_location_prefix\Az.IotHub -Force 
 } catch { Write-Host "Az.IotHub module failed force import"}
 
 try {Import-Module $module_location_prefix\Az.DeviceProvisioningServices -Cmdlet Add-AzIoTDeviceProvisioningServiceLinkedHub -Force 
