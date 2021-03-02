ec36afa6aba833e2a6c126a4e3e27ebe3417a800
Dane Walton <dawalton@microsoft.com>
2021-03-02 13:08:13 -0800

    scope it down

--- sdk/samples/iot/templates/iothub/test-resources-post.ps1
+++ sdk/samples/iot/templates/iothub/test-resources-post.ps1
@@ -21,7 +21,7 @@ if ($PSVersionTable.PSEdition -eq 'Desktop' -and (Get-Module -Name AzureRM -List
     Write-Warning -Message ('Az module not installed. Having both the AzureRM and ' +
       'Az modules installed at the same time is not supported.')
 } else {
-    Install-Module -Name Az -AllowClobber -Scope AllUsers -Force
+    Install-Module -Name Az -AllowClobber -Scope CurrentUser -Force
 }
 
 Import-Module -Name Az.IotHub
