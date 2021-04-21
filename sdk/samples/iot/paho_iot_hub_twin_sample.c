7b3ec62bff8f17c326e43832c0d60cd1dcac47c1
Mollie Munoz <momuno@microsoft.com>
2021-04-21 16:21:01 -0700

    Rever twin sample changes

--- sdk/samples/iot/paho_iot_hub_twin_sample.c
+++ sdk/samples/iot/paho_iot_hub_twin_sample.c
@@ -121,9 +121,7 @@ static void create_and_configure_mqtt_client(void)
       SAMPLE_TYPE, &env_vars, mqtt_endpoint_buffer, sizeof(mqtt_endpoint_buffer));
 
   // Initialize the hub client with the default connection options.
-  az_iot_hub_client_options options = az_iot_hub_client_options_default();
-
-  rc = az_iot_hub_client_init(&hub_client, env_vars.hub_hostname, env_vars.hub_device_id, &options);
+  rc = az_iot_hub_client_init(&hub_client, env_vars.hub_hostname, env_vars.hub_device_id, NULL);
   if (az_result_failed(rc))
   {
     IOT_SAMPLE_LOG_ERROR("Failed to initialize hub client: az_result return code 0x%08x.", rc);
