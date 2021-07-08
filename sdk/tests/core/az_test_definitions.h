ec4df09dc0ceb78054d9fd2ab085f6c71b6d997e
Ahson Khan <ahson_ahmedk@yahoo.com>
2021-07-08 14:26:47 -0700

    Add base64 encoding and decoding APIs that accept az_span. (#1816)
    * Add base64 encoding and decoding APIs that accept az_span.
    
    * Add entry to the changelog.
    
    * Address PR feedback - move const to r.h.s.

--- sdk/tests/core/az_test_definitions.h
+++ sdk/tests/core/az_test_definitions.h
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
 
+int test_az_base64();
 int test_az_context();
 int test_az_http();
 int test_az_json();
