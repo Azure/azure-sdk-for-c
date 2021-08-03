a9e97ccaae82f6494e2501401be7001b4f144ab9
Daniel Jurek <djurek@microsoft.com>
2021-08-03 12:49:12 -0700

    More logging

--- eng/scripts/vcpkg.ps1
+++ eng/scripts/vcpkg.ps1
@@ -16,18 +16,34 @@ Param (
 $initialDirectory = Get-Location
 
 try {
+    Write-Host "git clone https://github.com/Microsoft/vcpkg $VcpkgPath"
     git clone https://github.com/Microsoft/vcpkg $VcpkgPath
+
     Set-Location $VcpkgPath
+
+    Write-Host "git fetch --tags"
     git fetch --tags
+
+    Write-Host "git checkout $Ref"
     git checkout $Ref
 
     if ($IsWindows) {
+        Write-Host ".\bootstrap-vcpkg.bat"
         .\bootstrap-vcpkg.bat
+
+        Write-Host ".\vcpkg.exe install $($Dependencies.Split(' ')) --vcpkg-root $VcpkgPath"
         .\vcpkg.exe install $Dependencies.Split(' ') --vcpkg-root $VcpkgPath
     } else {
+        Write-Host "./bootstrap-vcpkg.sh"
         ./bootstrap-vcpkg.sh
+
+        Write-Host "./vcpkg install $($Dependencies.Split(' ')) --vcpkg-root $VcpkgPath"
         ./vcpkg install $Dependencies.Split(' ') --vcpkg-root $VcpkgPath
     }
+} catch {
+    Write-Host "Exception running vcpkg"
+    Write-Host $_.Exception
+    Write-Host $_.Exception.StackTrace
 } finally {
     Set-Location $initialDirectory
 }
