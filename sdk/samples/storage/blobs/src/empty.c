28bfd210da3061302506a79565528c35532d22ab
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2021-10-04 14:18:14 -0700

    Defined, but not used

--- sdk/samples/storage/blobs/src/empty.c
+++ sdk/samples/storage/blobs/src/empty.c
@@ -1,4 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
 
-static void f() {}
+static void g();
+static void f() { g(); }
+static void g() { f(); }
