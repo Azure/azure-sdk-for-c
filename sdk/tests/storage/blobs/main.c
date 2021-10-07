bd077b1466fcb068d7c11af3df54029263116e9d
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2021-10-07 13:11:30 -0700

    Enable upload test

--- sdk/tests/storage/blobs/main.c
+++ sdk/tests/storage/blobs/main.c
@@ -11,11 +11,13 @@
 #include <azure/core/_az_cfg.h>
 
 void test_storage_blobs_init(void** state);
+void test_storage_blobs_upload(void** state);
 
 int main(void)
 {
   const struct CMUnitTest tests[] = {
     cmocka_unit_test(test_storage_blobs_init),
+    cmocka_unit_test(test_storage_blobs_upload),
   };
 
   return cmocka_run_group_tests_name("az_storage_blobs", tests, NULL, NULL);
