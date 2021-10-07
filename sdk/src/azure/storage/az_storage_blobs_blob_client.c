545245fcaaa9e39b3ee982a7d42f6db2c8f6acb6
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2021-10-07 15:49:17 -0700

    Host

--- sdk/src/azure/storage/az_storage_blobs_blob_client.c
+++ sdk/src/azure/storage/az_storage_blobs_blob_client.c
@@ -49,6 +49,74 @@ AZ_NODISCARD az_storage_blobs_blob_client_options az_storage_blobs_blob_client_o
   return options;
 }
 
+AZ_INLINE az_span _az_get_host_from_url(az_span url)
+{
+  int32_t const url_length = az_span_size(url);
+  uint8_t const* url_ptr = az_span_ptr(url);
+
+  if (url_length < (sizeof("s://h") - 1))
+  {
+    int32_t const colon_max = url_length - (sizeof("//h") - 1);
+    for (int32_t colon_pos = 0; colon_pos < colon_max; ++colon_pos)
+    {
+      if (url_ptr[colon_pos] == ':')
+      {
+        if (url_ptr[colon_pos + 1] == '/' && url_ptr[colon_pos + 2] == '/')
+        {
+          int32_t const authority_pos = colon_pos + 3;
+          int32_t authority_end_pos = authority_pos;
+          for (; authority_end_pos < url_length; ++authority_end_pos)
+          {
+            if (url_ptr[authority_end_pos] == '/')
+            {
+              break;
+            }
+          }
+
+          if (authority_end_pos > authority_pos)
+          {
+            int32_t host_start_pos = authority_pos;
+            for (; host_start_pos < authority_end_pos; ++host_start_pos)
+            {
+              if (url_ptr[host_start_pos] == '@')
+              {
+                break;
+              }
+            }
+
+            if (host_start_pos == authority_end_pos)
+            {
+              host_start_pos = authority_pos;
+            }
+            else
+            {
+              ++host_start_pos;
+            }
+
+            if (host_start_pos < authority_end_pos)
+            {
+              int32_t host_end_pos = host_start_pos;
+              for (; host_end_pos < authority_end_pos; ++host_end_pos)
+              {
+                if (url_ptr[host_end_pos] == ':')
+                {
+                  break;
+                }
+              }
+
+              return az_span_slice(url, host_start_pos, host_end_pos);
+            }
+          }
+        }
+
+        break;
+      }
+    }
+  }
+
+  return AZ_SPAN_EMPTY;
+}
+
 AZ_NODISCARD az_result az_storage_blobs_blob_client_init(
     az_storage_blobs_blob_client* out_client,
     az_span endpoint,
@@ -63,6 +131,7 @@ AZ_NODISCARD az_result az_storage_blobs_blob_client_init(
   *out_client = (az_storage_blobs_blob_client) {
     ._internal = {
       .endpoint = AZ_SPAN_FROM_BUFFER(out_client->_internal.endpoint_buffer),
+      .host = AZ_SPAN_EMPTY,
       .options = *options,
       .credential = cred,
       .pipeline = (_az_http_pipeline){
@@ -117,6 +186,8 @@ AZ_NODISCARD az_result az_storage_blobs_blob_client_init(
   _az_RETURN_IF_NOT_ENOUGH_SIZE(out_client->_internal.endpoint, uri_size);
   az_span_copy(out_client->_internal.endpoint, endpoint);
   out_client->_internal.endpoint = az_span_slice(out_client->_internal.endpoint, 0, uri_size);
+
+  out_client->_internal.host = _az_get_host_from_url(out_client->_internal.endpoint);
 
   _az_RETURN_IF_FAILED(
       _az_credential_set_scopes(cred, AZ_SPAN_FROM_STR("https://storage.azure.com/.default")));
@@ -182,6 +253,13 @@ AZ_NODISCARD az_result az_storage_blobs_blob_upload(
   // add blob type to request
   _az_RETURN_IF_FAILED(az_http_request_append_header(
       &request, AZ_SPAN_FROM_STR("Content-Type"), AZ_SPAN_FROM_STR("text/plain")));
+
+  // add host header
+  if (az_span_size(ref_client->_internal.host) > 0)
+  {
+    _az_RETURN_IF_FAILED(az_http_request_append_header(
+        &request, AZ_SPAN_FROM_STR("Host"), ref_client->_internal.host));
+  }
 
   // start pipeline
   return az_http_pipeline_process(&ref_client->_internal.pipeline, &request, ref_response);
