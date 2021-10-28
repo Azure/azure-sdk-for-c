b993288dd5f144133a42f942336991169b27f6c4
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2021-10-27 20:43:14 -0700

    Experiment

--- sdk/samples/storage/blobs/src/http.c
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// SPDX-License-Identifier: MIT
-
-void core_span_create();
-
-void http_send_request();
-void http_send_request() { void core_span_create(); }
