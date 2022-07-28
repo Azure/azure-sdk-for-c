3d3f5c0a10af0bce25116a8f7d27c82b0ff01e75
George Arama <50641385+gearama@users.noreply.github.com>
2022-07-28 11:32:28 -0700

    the cmake of glory

---
+++ bld22/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/e/src/azure-sdk-for-c
+# Build directory: /mnt/e/src/azure-sdk-for-c/bld22
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("sdk/src/azure/core")
+subdirs("sdk/src/azure/iot")
+subdirs("sdk/src/azure/platform")
+subdirs("sdk/tests/core")
+subdirs("sdk/tests/iot/common")
+subdirs("sdk/tests/iot/hub")
+subdirs("sdk/tests/iot/provisioning")
---
+++ bld22/sdk/src/azure/platform/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/e/src/azure-sdk-for-c/sdk/src/azure/platform
+# Build directory: /mnt/e/src/azure-sdk-for-c/bld22/sdk/src/azure/platform
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
