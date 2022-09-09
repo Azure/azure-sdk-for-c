41ba8a0e8a797686ec0bcddd419be257dcae9be0
ewertons <ewertons@microsoft.com>
2022-09-09 13:05:17 -0700

    Fix spacing

--- sdk/src/azure/iot/az_iot_adu_client.c
+++ sdk/src/azure/iot/az_iot_adu_client.c
@@ -329,7 +329,7 @@ AZ_NODISCARD az_result az_iot_adu_client_get_agent_state_payload(
     _az_RETURN_IF_FAILED(az_json_writer_append_end_object(ref_json_writer));
   }
 
-  /* Fill installed update id.  */
+  /* Fill installed update id. */
   _az_RETURN_IF_FAILED(az_json_writer_append_property_name(
       ref_json_writer,
       AZ_SPAN_FROM_STR(AZ_IOT_ADU_CLIENT_AGENT_PROPERTY_NAME_INSTALLED_UPDATE_ID)));
