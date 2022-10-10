7bd309851c0b2eb0560b6641e222ffa6304c9c10
Dane Walton <dawalton@microsoft.com>
2022-10-10 13:01:36 -0500

    add separate

--- sdk/tests/iot/adu/test_az_iot_adu.c
+++ sdk/tests/iot/adu/test_az_iot_adu.c
@@ -41,6 +41,16 @@ static uint8_t expected_agent_state_long_payload[]
       "0.8.0-rc1-public-preview\"},\"compatPropertyNames\":\"manufacturer,model\","
       "\"lastInstallResult\":{\"resultCode\":0,\"extendedResultCode\":1234,\"resultDetails\":"
       "\"Ok\",\"step_0\":{\"resultCode\":0,\"extendedResultCode\":1234,\"resultDetails\":\"Ok\"}},"
+      "\"state\":0,\"workflow\":{\"action\":3,\"id\":\"51552a54-765e-419f-892a-c822549b6f38\"},"
+      "\"installedUpdateId\":\"{\\\"provider\\\":\\\"Contoso\\\",\\\"name\\\":\\\"Foobar\\\","
+      "\\\"version\\\":\\\"1.0\\\"}\"}}}";
+static uint8_t expected_agent_state_long_payload_with_retry[]
+    = "{\"deviceUpdate\":{\"__t\":\"c\",\"agent\":{\"deviceProperties\":{\"manufacturer\":"
+      "\"Contoso\",\"model\":\"Foobar\",\"interfaceId\":\"dtmi:azure:iot:deviceUpdate;1\","
+      "\"aduVer\":\"DU;agent/"
+      "0.8.0-rc1-public-preview\"},\"compatPropertyNames\":\"manufacturer,model\","
+      "\"lastInstallResult\":{\"resultCode\":0,\"extendedResultCode\":1234,\"resultDetails\":"
+      "\"Ok\",\"step_0\":{\"resultCode\":0,\"extendedResultCode\":1234,\"resultDetails\":\"Ok\"}},"
       "\"state\":0,\"workflow\":{\"action\":3,\"id\":\"51552a54-765e-419f-892a-c822549b6f38\","
       "\"retryTimestamp\":\"2022-01-26T11:33:29.9680598Z\"},"
       "\"installedUpdateId\":\"{\\\"provider\\\":\\\"Contoso\\\",\\\"name\\\":\\\"Foobar\\\","
@@ -63,6 +73,49 @@ static az_span result_details = AZ_SPAN_LITERAL_FROM_STR("Ok");
 
 /*Request Values */
 static uint8_t adu_request_payload[]
+    = "{\"service\":{\"workflow\":{\"action\":3,\"id\":\"51552a54-765e-419f-892a-c822549b6f38\"},"
+      "\"updateManifest\":\"{\\\"manifestVersion\\\":\\\"4\\\",\\\"updateId\\\":{\\\"provider\\\":"
+      "\\\"Contoso\\\",\\\"name\\\":\\\"Foobar\\\",\\\"version\\\":\\\"1.1\\\"},"
+      "\\\"compatibility\\\":[{\\\"deviceManufacturer\\\":\\\"Contoso\\\",\\\"deviceModel\\\":"
+      "\\\"Foobar\\\"}],\\\"instructions\\\":{\\\"steps\\\":[{\\\"handler\\\":\\\"microsoft/"
+      "swupdate:1\\\",\\\"files\\\":[\\\"f2f4a804ca17afbae\\\"],\\\"handlerProperties\\\":{"
+      "\\\"installedCriteria\\\":\\\"1.0\\\"}}]},\\\"files\\\":{\\\"f2f4a804ca17afbae\\\":{"
+      "\\\"fileName\\\":\\\"iot-middleware-sample-adu-v1.1\\\",\\\"sizeInBytes\\\":844976,"
+      "\\\"hashes\\\":{\\\"sha256\\\":\\\"xsoCnYAMkZZ7m9RL9Vyg9jKfFehCNxyuPFaJVM/"
+      "WBi0=\\\"}}},\\\"createdDateTime\\\":\\\"2022-07-07T03:02:48.8449038Z\\\"}\","
+      "\"updateManifestSignature\":"
+      "\"eyJhbGciOiJSUzI1NiIsInNqd2siOiJleUpoYkdjaU9pSlNVekkxTmlJc0ltdHBaQ0k2SWtGRVZTNHlNREEzTURJdV"
+      "VpSjkuZXlKcmRIa2lPaUpTVTBFaUxDSnVJam9pYkV4bWMwdHZPRmwwWW1Oak1sRXpUalV3VlhSTVNXWlhVVXhXVTBGRl"
+      "ltTm9LMFl2WTJVM1V6Rlpja3BvV0U5VGNucFRaa051VEhCVmFYRlFWSGMwZWxndmRHbEJja0ZGZFhrM1JFRmxWVzVGU0"
+      "VWamVEZE9hM2QzZVRVdk9IcExaV3AyWTBWWWNFRktMMlV6UWt0SE5FVTBiMjVtU0ZGRmNFOXplSGRQUzBWbFJ6Qkhkam"
+      "wzVjB3emVsUmpUblprUzFoUFJGaEdNMVZRWlVveGIwZGlVRkZ0Y3pKNmJVTktlRUppZEZOSldVbDBiWFpwWTNneVpXdG"
+      "tWbnBYUm5jdmRrdFVUblZMYXpob2NVczNTRkptYWs5VlMzVkxXSGxqSzNsSVVVa3dZVVpDY2pKNmEyc3plR2d4ZEVWUF"
+      "N6azRWMHBtZUdKamFsQnpSRTgyWjNwWmVtdFlla05OZW1Fd1R6QkhhV0pDWjB4QlZGUTVUV1k0V1ZCd1dVY3lhblpQWV"
+      "VSVmIwTlJiakpWWTFWU1RtUnNPR2hLWW5scWJscHZNa3B5SzFVNE5IbDFjVTlyTjBZMFdubFRiMEoyTkdKWVNrZ3lXbE"
+      "pTV2tab0wzVlRiSE5XT1hkU2JWbG9XWEoyT1RGRVdtbHhhemhJVWpaRVUyeHVabTVsZFRJNFJsUm9SVzF0YjNOVlRUTn"
+      "JNbGxNYzBKak5FSnZkWEIwTTNsaFNEaFpia3BVTnpSMU16TjFlakU1TDAxNlZIVnFTMmMzVkdGcE1USXJXR0owYmxwRU"
+      "9XcFVSMkY1U25Sc2FFWmxWeXRJUXpVM1FYUkJSbHBvY1ZsM2VVZHJXQ3M0TTBGaFVGaGFOR0V4VHpoMU1qTk9WVWQxTW"
+      "tGd04yOU5NVTR3ZVVKS0swbHNUM29pTENKbElqb2lRVkZCUWlJc0ltRnNaeUk2SWxKVE1qVTJJaXdpYTJsa0lqb2lRVV"
+      "JWTGpJeE1EWXdPUzVTTGxNaWZRLlJLS2VBZE02dGFjdWZpSVU3eTV2S3dsNFpQLURMNnEteHlrTndEdkljZFpIaTBIa2"
+      "RIZ1V2WnoyZzZCTmpLS21WTU92dXp6TjhEczhybXo1dnMwT1RJN2tYUG1YeDZFLUYyUXVoUXNxT3J5LS1aN2J3TW5LYT"
+      "NkZk1sbkthWU9PdURtV252RWMyR0hWdVVTSzREbmw0TE9vTTQxOVlMNThWTDAtSEthU18xYmNOUDhXYjVZR08xZXh1Rm"
+      "piVGtIZkNIU0duVThJeUFjczlGTjhUT3JETHZpVEtwcWtvM3RiSUwxZE1TN3NhLWJkZExUVWp6TnVLTmFpNnpIWTdSan"
+      "ZGbjhjUDN6R2xjQnN1aVQ0XzVVaDZ0M05rZW1UdV9tZjdtZUFLLTBTMTAzMFpSNnNTR281azgtTE1sX0ZaUmh4djNFZF"
+      "NtR2RBUTNlMDVMRzNnVVAyNzhTQWVzWHhNQUlHWmcxUFE3aEpoZGZHdmVGanJNdkdTSVFEM09wRnEtZHREcEFXbUo2Zm"
+      "5sZFA1UWxYek5tQkJTMlZRQUtXZU9BYjh0Yjl5aVhsemhtT1dLRjF4SzlseHpYUG9GNmllOFRUWlJ4T0hxTjNiSkVISk"
+      "VoQmVLclh6YkViV2tFNm4zTEoxbkd5M1htUlVFcER0Umdpa0tBUzZybFhFT0VneXNjIn0."
+      "eyJzaGEyNTYiOiJiUlkrcis0MzdsYTV5d2hIeDdqVHhlVVRkeDdJdXQyQkNlcVpoQys5bmFNPSJ9."
+      "eYoBoq9EOiCebTJAMhRh9DARC69F3C4Qsia86no9YbMJzwKt-rH88Va4dL59uNTlPNBQid4u0RlXSUTuma_v-"
+      "Sf4hyw70tCskwru5Fp41k9Ve3YSkulUKzctEhaNUJ9tUSA11Tz9HwJHOAEA1-S_dXWR_yuxabk9G_"
+      "BiucsuKhoI0Bas4e1ydQE2jXZNdVVibrFSqxvuVZrxHKVhwm-"
+      "G9RYHjZcoSgmQ58vWyaC2l8K8ZqnlQWmuLur0CZFQlanUVxDocJUtu1MnB2ER6emMRD_"
+      "4Azup2K4apq9E1EfYBbXxOZ0N5jaSr-2xg8NVSow5NqNSaYYY43wy_NIUefRlbSYu5zOrSWtuIwRdsO-"
+      "43Eo8b9vuJj1Qty9ee6xz1gdUNHnUdnM6dHEplZK0GZznsxRviFXt7yv8bVLd32Z7QDtFh3s17xlKulBZxWP-"
+      "q96r92RoUTov2M3ynPZSDmc6Mz7-r8ioO5VHO5pAPCH-tF5zsqzipPJKmBMaf5gYk8wR\",\"fileUrls\":{"
+      "\"f2f4a804ca17afbae\":\"http://contoso-adu-instance--contoso-adu.b.nlu.dl.adu.microsoft.com/"
+      "westus2/contoso-adu-instance--contoso-adu/67c8d2ef5148403391bed74f51a28597/"
+      "iot-middleware-sample-adu-v1.1\"}}}";
+static uint8_t adu_request_payload_with_retry[]
     = "{\"service\":{\"workflow\":{\"action\":3,\"id\":\"51552a54-765e-419f-892a-c822549b6f38\","
       "\"retryTimestamp\":\"2022-01-26T11:33:29.9680598Z\"},"
       "\"updateManifest\":\"{\\\"manifestVersion\\\":\\\"4\\\",\\\"updateId\\\":{\\\"provider\\\":"
@@ -567,6 +620,61 @@ static void test_az_iot_adu_client_get_agent_state_long_payload_succeed(void** s
       sizeof(expected_agent_state_long_payload) - 1);
 }
 
+static void test_az_iot_adu_client_get_agent_state_long_payload_with_retry_succeed(void** state)
+{
+  (void)state;
+
+  az_iot_adu_client client;
+  az_iot_adu_client_update_request request;
+  az_iot_adu_client_install_result install_result;
+  az_json_writer jw;
+  az_json_reader jr;
+  uint8_t payload_buffer[TEST_SPAN_BUFFER_SIZE];
+
+  install_result.extended_result_code = extended_result_code;
+  install_result.result_code = result_code;
+  install_result.result_details = result_details;
+  install_result.step_results_count = 1;
+  install_result.step_results[0].result_code = result_code;
+  install_result.step_results[0].extended_result_code = extended_result_code;
+  install_result.step_results[0].result_details = result_details;
+
+  assert_int_equal(az_iot_adu_client_init(&client, NULL), AZ_OK);
+
+  assert_int_equal(
+      az_json_reader_init(
+          &jr,
+          az_span_create(
+              adu_request_payload_with_retry, sizeof(adu_request_payload_with_retry) - 1),
+          NULL),
+      AZ_OK);
+
+  // parse_service_properties requires that the reader be placed on the "service" prop name
+  assert_int_equal(az_json_reader_next_token(&jr), AZ_OK);
+  assert_int_equal(az_json_reader_next_token(&jr), AZ_OK);
+
+  assert_int_equal(az_iot_adu_client_parse_service_properties(&client, &jr, &request), AZ_OK);
+
+  assert_int_equal(
+      az_json_writer_init(&jw, az_span_create(payload_buffer, sizeof(payload_buffer)), NULL),
+      AZ_OK);
+
+  assert_int_equal(
+      az_iot_adu_client_get_agent_state_payload(
+          &client,
+          &adu_device_properties,
+          AZ_IOT_ADU_CLIENT_AGENT_STATE_IDLE,
+          &request.workflow,
+          &install_result,
+          &jw),
+      AZ_OK);
+
+  assert_memory_equal(
+      payload_buffer,
+      expected_agent_state_long_payload_with_retry,
+      sizeof(expected_agent_state_long_payload_with_retry) - 1);
+}
+
 static void test_az_iot_adu_client_get_service_properties_response_succeed(void** state)
 {
   (void)state;
@@ -605,6 +713,61 @@ static void test_az_iot_adu_client_parse_service_properties_succeed(void** state
   assert_int_equal(
       az_json_reader_init(
           &reader, az_span_create(adu_request_payload, sizeof(adu_request_payload) - 1), NULL),
+      AZ_OK);
+
+  // parse_service_properties requires that the reader be placed on the "service" prop name
+  assert_int_equal(az_json_reader_next_token(&reader), AZ_OK);
+  assert_int_equal(az_json_reader_next_token(&reader), AZ_OK);
+
+  assert_int_equal(
+      az_iot_adu_client_parse_service_properties(&adu_client, &reader, &request), AZ_OK);
+
+  // Workflow
+  assert_int_equal(request.workflow.action, workflow_action);
+  assert_memory_equal(az_span_ptr(request.workflow.id), workflow_id, sizeof(workflow_id) - 1);
+  assert_null(az_span_ptr(request.workflow.retry_timestamp));
+  assert_int_equal(az_span_size(request.workflow.retry_timestamp), 0);
+
+  // Update Manifest
+  int32_t out_size;
+  assert_int_equal(
+      az_json_string_unescape(
+          request.update_manifest, (char*)scratch_buffer, sizeof(scratch_buffer), &out_size),
+      AZ_OK);
+  request.update_manifest = az_span_create(scratch_buffer, out_size);
+  assert_memory_equal(
+      az_span_ptr(request.update_manifest), adu_request_manifest, sizeof(adu_request_manifest) - 1);
+  assert_int_equal(az_span_size(request.update_manifest), sizeof(adu_request_manifest) - 1);
+
+  // Signature
+  assert_memory_equal(
+      az_span_ptr(request.update_manifest_signature), valid_signature, sizeof(valid_signature) - 1);
+  assert_int_equal(az_span_size(request.update_manifest_signature), sizeof(valid_signature) - 1);
+
+  // File URLs
+  assert_memory_equal(az_span_ptr(request.file_urls[0].id), files_id, sizeof(files_id) - 1);
+  assert_int_equal(az_span_size(request.file_urls[0].id), sizeof(files_id) - 1);
+  assert_memory_equal(az_span_ptr(request.file_urls[0].url), file_url, sizeof(file_url) - 1);
+  assert_int_equal(az_span_size(request.file_urls[0].url), sizeof(file_url) - 1);
+  assert_int_equal(request.file_urls_count, 1);
+}
+
+static void test_az_iot_adu_client_parse_service_properties_with_retry_succeed(void** state)
+{
+  (void)state;
+
+  az_iot_adu_client adu_client;
+  az_json_reader reader;
+  az_iot_adu_client_update_request request;
+
+  assert_int_equal(az_iot_adu_client_init(&adu_client, NULL), AZ_OK);
+
+  assert_int_equal(
+      az_json_reader_init(
+          &reader,
+          az_span_create(
+              adu_request_payload_with_retry, sizeof(adu_request_payload_with_retry) - 1),
+          NULL),
       AZ_OK);
 
   // parse_service_properties requires that the reader be placed on the "service" prop name
@@ -1041,8 +1204,10 @@ int test_az_iot_adu()
     cmocka_unit_test(test_az_iot_adu_is_component_device_update_succeed),
     cmocka_unit_test(test_az_iot_adu_client_get_agent_state_payload_succeed),
     cmocka_unit_test(test_az_iot_adu_client_get_agent_state_long_payload_succeed),
+    cmocka_unit_test(test_az_iot_adu_client_get_agent_state_long_payload_with_retry_succeed),
     cmocka_unit_test(test_az_iot_adu_client_get_service_properties_response_succeed),
     cmocka_unit_test(test_az_iot_adu_client_parse_service_properties_succeed),
+    cmocka_unit_test(test_az_iot_adu_client_parse_service_properties_with_retry_succeed),
     cmocka_unit_test(
         test_az_iot_adu_client_parse_service_properties_with_null_file_url_value_succeed),
     cmocka_unit_test(
