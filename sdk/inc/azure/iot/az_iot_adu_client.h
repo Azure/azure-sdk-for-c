bebf530cfd040f1ddac98b02673f5386f3abc82e
Dane Walton <dawalton@microsoft.com>
2022-11-02 11:10:05 -0400

    move configurable values to the config

--- sdk/inc/azure/iot/az_iot_adu_client.h
+++ sdk/inc/azure/iot/az_iot_adu_client.h
@@ -22,6 +22,7 @@
 #include <azure/core/az_json.h>
 #include <azure/core/az_result.h>
 #include <azure/core/az_span.h>
+#include <azure/iot/az_iot_config.h>
 #include <azure/iot/az_iot_hub_client.h>
 
 #include <stdbool.h>
@@ -44,60 +45,31 @@
  */
 #define AZ_IOT_ADU_CLIENT_PROPERTIES_COMPONENT_NAME "deviceUpdate"
 
-/**
- * @brief ADU Service Response (Accepted)
- */
-#define AZ_IOT_ADU_CLIENT_REQUEST_ACCEPTED 200
+enum
+{
+  /// ADU Service Response (Accepted)
+  AZ_IOT_ADU_CLIENT_REQUEST_ACCEPTED = 200,
+  /// ADU Service Response (Rejected)
+  AZ_IOT_ADU_CLIENT_REQUEST_REJECTED = 406
+};
 
-/**
- * @brief ADU Service Response (Rejected)
- */
-#define AZ_IOT_ADU_CLIENT_REQUEST_REJECTED 406
+enum
+{
+  /// ADU Service Action (Apply)
+  AZ_IOT_ADU_CLIENT_SERVICE_ACTION_APPLY_DEPLOYMENT = 3,
+  /// ADU Service Action (Cancel)
+  AZ_IOT_ADU_CLIENT_SERVICE_ACTION_CANCEL = 255
+};
 
-/**
- * @brief ADU Service Action (Apply)
- */
-#define AZ_IOT_ADU_CLIENT_SERVICE_ACTION_APPLY_DEPLOYMENT 3
-
-/**
- * @brief ADU Service Action (Cancel)
- */
-#define AZ_IOT_ADU_CLIENT_SERVICE_ACTION_CANCEL 255
-
-/**
- * @brief ADU Agent State (Idle)
- */
-#define AZ_IOT_ADU_CLIENT_AGENT_STATE_IDLE 0
-
-/**
- * @brief ADU Agent State (In Progress)
- */
-#define AZ_IOT_ADU_CLIENT_AGENT_STATE_DEPLOYMENT_IN_PROGRESS 6
-
-/**
- * @brief ADU Agent State (Failed)
- */
-#define AZ_IOT_ADU_CLIENT_AGENT_STATE_FAILED 255
-
-/**
- * @brief  Maximum Number of Files Handled by this ADU Agent (Number of URLs)
- */
-#define AZ_IOT_ADU_CLIENT_MAX_FILE_URL_COUNT 10
-
-/**
- * @brief  Maximum Number of Files Handled by this ADU Agent (Steps)
- */
-#define AZ_IOT_ADU_CLIENT_MAX_INSTRUCTIONS_STEPS 10
-
-/**
- * @brief  Maximum Number of Files Handled by this ADU Agent (File Hashes)
- */
-#define AZ_IOT_ADU_CLIENT_MAX_FILE_HASH_COUNT 2
-
-/**
- * @brief Maximum Number of Custom Device Properties
- */
-#define AZ_IOT_ADU_CLIENT_MAX_DEVICE_CUSTOM_PROPERTIES 5
+enum
+{
+  /// ADU Agent State (Idle)
+  AZ_IOT_ADU_CLIENT_AGENT_STATE_IDLE = 0,
+  /// ADU Agent State (In Progress)
+  AZ_IOT_ADU_CLIENT_AGENT_STATE_DEPLOYMENT_IN_PROGRESS = 6,
+  /// ADU Agent State (Failed)
+  AZ_IOT_ADU_CLIENT_AGENT_STATE_FAILED = 255
+};
 
 /**
  * @brief Default Agent Compatibility Properties
@@ -271,7 +243,7 @@ typedef struct
 {
   /**
    * An integer that corresponds to an action the agent should perform.
-   * @remark Refer to the following defines for the expected values:
+   * @remark Refer to the following values for the expected values:
    *         AZ_IOT_ADU_CLIENT_SERVICE_ACTION_APPLY_DEPLOYMENT
    *         AZ_IOT_ADU_CLIENT_SERVICE_ACTION_CANCEL
    */
---
+++ sdk/inc/azure/iot/az_iot_config.h
@@ -0,0 +1,40 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+/**
+ * @file
+ *
+ * @brief Configurable constants used by the Azure IoT SDK.
+ *
+ * @remarks Typically, these constants do not need to be modified but depending on how your
+ * application uses an Azure service, they can be adjusted.
+ *
+ * @note You MUST NOT use any symbols (macros, functions, structures, enums, etc.)
+ * prefixed with an underscore ('_') directly in your application code. These symbols
+ * are part of Azure SDK's internal implementation; we do not document these symbols
+ * and they are subject to change in future versions of the SDK which would break your code.
+ */
+
+#ifndef _az_IOT_CONFIG_H
+#define _az_IOT_CONFIG_H
+
+#include <azure/core/_az_cfg_prefix.h>
+
+enum
+{
+  /// Maximum Number of Files Handled by this ADU Agent (Number of URLs)
+  AZ_IOT_ADU_CLIENT_MAX_FILE_URL_COUNT = 10,
+
+  /// Maximum Number of Files Handled by this ADU Agent (Steps)
+  AZ_IOT_ADU_CLIENT_MAX_INSTRUCTIONS_STEPS = 10,
+
+  /// Maximum Number of Files Handled by this ADU Agent (File Hashes)
+  AZ_IOT_ADU_CLIENT_MAX_FILE_HASH_COUNT = 2,
+
+  /// Maximum Number of Custom Device Properties
+  AZ_IOT_ADU_CLIENT_MAX_DEVICE_CUSTOM_PROPERTIES = 5
+};
+
+#include <azure/core/_az_cfg_suffix.h>
+
+#endif // _az_IOT_CONFIG_H
