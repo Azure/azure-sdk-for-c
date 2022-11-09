9e43af18a41b624c8dc51ae2af2f81af77777ad7
Dane Walton <dawalton@microsoft.com>
2022-11-09 10:25:38 -0500

    add link to dtmi

--- sdk/inc/azure/iot/az_iot_adu_client.h
+++ sdk/inc/azure/iot/az_iot_adu_client.h
@@ -32,6 +32,8 @@
 
 /**
  * @brief  Define the ADU agent model ID.
+ * 
+ * https://github.com/Azure/iot-plugandplay-models/blob/main/dtmi/azure/iot/deviceupdatecontractmodel-2.json
  */
 #define AZ_IOT_ADU_CLIENT_AGENT_MODEL_ID "dtmi:azure:iot:deviceUpdateContractModel;2"
 
