68f231a42e692801912f185e68fde9895ef83704
Dane Walton <dawalton@microsoft.com>
2022-11-09 13:57:40 -0500

    update to GA dtmi (#2410)

--- sdk/inc/azure/iot/az_iot_adu_client.h
+++ sdk/inc/azure/iot/az_iot_adu_client.h
@@ -32,8 +32,10 @@
 
 /**
  * @brief  Define the ADU agent model ID.
+ *
+ * https://github.com/Azure/iot-plugandplay-models/blob/main/dtmi/azure/iot/deviceupdatecontractmodel-2.json
  */
-#define AZ_IOT_ADU_CLIENT_AGENT_MODEL_ID "dtmi:azure:iot:deviceUpdateModel;1"
+#define AZ_IOT_ADU_CLIENT_AGENT_MODEL_ID "dtmi:azure:iot:deviceUpdateContractModel;2"
 
 /**
  * @brief ADU Agent Version
