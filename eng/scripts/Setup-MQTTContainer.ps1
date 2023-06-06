56b1871ceeb39bc335f702c8584e93728d203b8e
RLeclair <raulleclair@microsoft.com>
2023-06-06 00:06:19 +0000

    Removing docker installation, apt dependencies

--- eng/scripts/Setup-MQTTContainer.ps1
+++ eng/scripts/Setup-MQTTContainer.ps1
@@ -6,23 +6,6 @@ param (
 )
 
 if ($IsLinux -and $AgentImage -match "ubuntu") {
-    $dockerGpgDir = "/etc/apt/keyrings"
-    $dockerGpgPath = "/etc/apt/keyrings/docker.gpg"
-    $dockerGpgUrl = "https://download.docker.com/linux/ubuntu/gpg"
-    $dockerListPath = "/etc/apt/sources.list.d/docker.list"
-
-    sudo install -m 0755 -d $dockerGpgDir
-
-    sudo curl -fsSL $dockerGpgUrl | sudo gpg --dearmor -o $dockerGpgPath
-
-    sudo chmod a+r /etc/apt/keyrings/docker.gpg
-    
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=$($dockerGpgPath)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee $dockerListPath > /dev/null
-
-    sudo apt-get update
-
-    sudo apt-get -y install docker-ce
-
     docker pull eclipse-mosquitto
 
     sudo docker run -d -p 1883:1883 -p 9001:9001 -v ${$ConfigFile}:/mosquitto/config/mosquitto.conf eclipse-mosquitto
@@ -36,4 +19,6 @@ if ($IsLinux -and $AgentImage -match "ubuntu") {
     sudo netstat -p
 
     sudo netstat -apn | grep 1883
+
+    docker ps
 }
