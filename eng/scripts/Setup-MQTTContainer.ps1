3ca2014606a1a6fc1334d39b73c7de6b08230505
RLeclair <raulleclair@microsoft.com>
2023-06-22 21:06:31 +0000

    Removing debugging code. Fixing minor bug.

--- eng/scripts/Setup-MQTTContainer.ps1
+++ eng/scripts/Setup-MQTTContainer.ps1
@@ -8,33 +8,11 @@ param (
 if ($IsLinux -and $AgentImage -match "ubuntu") {
     docker pull azsdkengsys.azurecr.io/eclipse-mosquitto:2.0.1
 
-    Write-Host "Following is the config file path: $ConfigFile"
+    $CurrentPath = Get-Location
 
-    # Get the current path
-    $currentPath = Get-Location
+    $FullPathConfigFile = Resolve-Path (Join-Path $CurrentPath $ConfigFile)
 
-    # Print the current path
-    Write-Host "Current path: $currentPath"
-
-    # Get the files and folders in the current directory
-    $filesAndFolders = Get-ChildItem -Path $currentPath
-
-    # Print the files and folders
-    Write-Host "Files and folders in current directory:"
-    foreach ($item in $filesAndFolders) {
-        Write-Host $item.Name
-    }
-
-    Write-Host "Run the docker run command to start the container"
-
-    $fullPathConfigFile = "$currentPath/$ConfigFile"
-
-    Write-Host "Full path of the config file: $fullPathConfigFile"
-
-    #sudo docker run -d -p 127.0.0.1:2883:2883 -p 127.0.0.1:9001:9001 -v ${$fullPathConfigFile}:/mosquitto/config/mosquitto.conf azsdkengsys.azurecr.io/eclipse-mosquitto:2.0.1
-    sudo docker run -d -p 127.0.0.1:2883:2883 -p 127.0.0.1:9001:9001 --mount type=bind,src=$fullPathConfigFile,dst=/mosquitto/config/mosquitto.conf azsdkengsys.azurecr.io/eclipse-mosquitto:2.0.1
-
-    sudo docker ps -a
+    sudo docker run -d -p 127.0.0.1:2883:2883 -p 127.0.0.1:9001:9001 --mount type=bind,src=$FullPathConfigFile,dst=/mosquitto/config/mosquitto.conf azsdkengsys.azurecr.io/eclipse-mosquitto:2.0.1
 
     Start-Sleep -Milliseconds 2000
 
@@ -42,24 +20,22 @@ if ($IsLinux -and $AgentImage -match "ubuntu") {
         $container = sudo docker ps -a --filter "ancestor=azsdkengsys.azurecr.io/eclipse-mosquitto:2.0.1" --format "{{.Names}}"
     
         if ($container) {
-            Write-Host "Container is running"
+            Write-Host "Container started."
 
-            # Debugging
-            sudo docker container inspect $container
-            Write-Host "<<<<<<<<<<< Printing out logs >>>>>>>>>>>>>"
+            Write-Host "Container initialization logs:"
             sudo docker logs $container
 
             break
         }
         else {
-            Write-Host "Container is not running"
+            Write-Host "Container starting..."
             Start-Sleep -Milliseconds 2000
             $count++
         }
     }
 
     if ($count -eq 3) {
-        Write-Error "Container is not running"
+        Write-Error "Container failed to start."
         exit 1
     }
 }
