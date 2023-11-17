3f7f45cfd0e3cda0e5f85113e423dcd8cd5ae32c
Cristian Pop <CIPop@users.noreply.github.com>
2023-11-17 11:32:27 -0800

    HFSM and Async Pipeline documentation. (#2689)
    * HFSM and Async Pipeline documentation.
    
    * Update sdk/docs/core/az_event_pipeline.md
    
    Co-authored-by: Valerie Avva Lim <54871851+vaavva@users.noreply.github.com>
    
    * Addressing feedback.
    
    * Apply suggestions from code review
    
    Co-authored-by: Valerie Avva Lim <54871851+vaavva@users.noreply.github.com>
    
    * Update sdk/docs/core/az_event_pipeline.md
    
    Co-authored-by: Raul Leclair <raulleclair@microsoft.com>
    
    * Addressing feedback.
    
    * Apply suggestions from code review
    
    Co-authored-by: Raul Leclair <raulleclair@microsoft.com>
    
    ---------
    
    Co-authored-by: Valerie Avva Lim <54871851+vaavva@users.noreply.github.com>
    Co-authored-by: Raul Leclair <raulleclair@microsoft.com>

--- sdk/inc/azure/core/az_mqtt5_rpc_server.h
+++ sdk/inc/azure/core/az_mqtt5_rpc_server.h
@@ -6,7 +6,7 @@
  *
  * @brief Definition of #az_mqtt5_rpc_server. You use the RPC server to receive commands.
  *
- * @note The state diagram is in sdk/docs/core/rpc_server.puml
+ * @note The state diagram is in sdk/docs/core/mqtt_rpc_server.puml
  *
  * @note You MUST NOT use any symbols (macros, functions, structures, enums, etc.)
  * prefixed with an underscore ('_') directly in your application code. These symbols
