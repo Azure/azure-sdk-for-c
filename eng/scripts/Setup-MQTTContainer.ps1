0bb459698076b3e9441437add8638648719a7ad5
RLeclair <raulleclair@microsoft.com>
2024-03-04 23:01:12 +0000

    Addressing comments

--- eng/scripts/Setup-MQTTContainer.ps1
+++ eng/scripts/Setup-MQTTContainer.ps1
@@ -6,43 +6,38 @@ param (
 )
 
 if ($IsLinux -and $AgentImage -match "ubuntu") {
-    $FullCurrentPath = Get-Location | Resolve-Path
-    
     # Setup certificates
     $CAPass = 1122
 
     # CA Generation
-    Invoke-Expression "sudo openssl genrsa -passout pass:$($CAPass) -des3 -out ca.key 2048"
-    Invoke-Expression "sudo openssl req -new -x509 -days 1826 -key ca.key -out ca.pem -subj `"/O=CA-cert/CN=127.0.0.1`" -passin pass:$($CAPass)"
+    sudo openssl genrsa -passout pass:$CAPass -des3 -out ca.key 2048
+    sudo openssl req -new -x509 -days 1826 -key ca.key -out ca.pem -subj /O=CA-cert/CN=127.0.0.1 -passin pass:$CAPass
 
     # Server Certificate Generation
-    Invoke-Expression "sudo openssl genrsa -out server.key 2048"
-    Invoke-Expression "sudo openssl req -new -out server.csr -key server.key -subj `"/O=SERVER-cert/CN=127.0.0.1`""
-    Invoke-Expression "sudo openssl x509 -req -in server.csr -CA ca.pem -CAkey ca.key -CAcreateserial -out server.pem -days 360 -passin pass:$($CAPass)"
+    sudo openssl genrsa -out server.key 2048
+    sudo openssl req -new -out server.csr -key server.key -subj /O=SERVER-cert/CN=127.0.0.1
+    sudo openssl x509 -req -in server.csr -CA ca.pem -CAkey ca.key -CAcreateserial -out server.pem -days 360 -passin pass:$CAPass
 
     # Client Certificate Generation
-    Invoke-Expression "sudo openssl genrsa -out client-key.pem 2048"
-    Invoke-Expression "sudo openssl req -new -out client.csr -key client-key.pem -subj `"/O=CLIENT-cert/CN=127.0.0.1`""
-    Invoke-Expression "sudo openssl x509 -req -in client.csr -CA ca.pem -CAkey ca.key -CAcreateserial -out client.pem -days 360 -passin pass:$($CAPass)"
+    sudo openssl genrsa -out client-key.pem 2048
+    sudo openssl req -new -out client.csr -key client-key.pem -subj /O=CLIENT-cert/CN=127.0.0.1
+    sudo openssl x509 -req -in client.csr -CA ca.pem -CAkey ca.key -CAcreateserial -out client.pem -days 360 -passin pass:$CAPass
 
     # Setting Permissions
-    Invoke-Expression "sudo chmod a+r $($FullCurrentPath)/server.key"
-    Invoke-Expression "sudo chmod a+r $($FullCurrentPath)/client.pem"
-    Invoke-Expression "sudo chmod a+r $($FullCurrentPath)/client-key.pem"
-    Invoke-Expression "sudo chmod a+r $($FullCurrentPath)/ca.key"
+    sudo chmod a+r (Resolve-Path server.key)
+    sudo chmod a+r (Resolve-Path client.pem)
+    sudo chmod a+r (Resolve-Path client-key.pem)
+    sudo chmod a+r (Resolve-Path ca.key)
 
     # Copying Certificates to the right location
-    Invoke-Expression "mkdir certs"
-    $FullPathCerts = (Join-Path $FullCurrentPath "certs/")
-    Invoke-Expression "sudo cp $($FullCurrentPath)/ca.pem $($FullPathCerts)/ca.pem"
-    Invoke-Expression "sudo cp $($FullCurrentPath)/server.pem $($FullPathCerts)/server.pem"
-    Invoke-Expression "sudo cp $($FullCurrentPath)/server.key $($FullPathCerts)/server.key"
+    $certsPath = New-Item -ItemType Directory -Path certs
+    sudo cp (Resolve-Path ca.pem) $certsPath/ca.pem
+    sudo cp (Resolve-Path server.pem) $certsPath/server.pem
+    sudo cp (Resolve-Path server.key) $certsPath/server.key
 
     docker pull azsdkengsys.azurecr.io/eclipse-mosquitto:2.0.1
 
-    $FullPathConfigFile = (Join-Path $FullCurrentPath $ConfigFile)
-
-    Invoke-Expression "sudo docker run -d -p 127.0.0.1:8883:8883 -p 127.0.0.1:2883:2883 --mount type=bind,src=$($FullPathConfigFile),dst=/mosquitto/config/mosquitto.conf --mount type=bind,src=$($FullPathCerts),dst=/mosquitto/config/certs azsdkengsys.azurecr.io/eclipse-mosquitto:2.0.1"
+    Invoke-Expression "sudo docker run -d -p 127.0.0.1:8883:8883 -p 127.0.0.1:2883:2883 --mount type=bind,src=$(Resolve-Path $ConfigFile),dst=/mosquitto/config/mosquitto.conf --mount type=bind,src=$certsPath,dst=/mosquitto/config/certs azsdkengsys.azurecr.io/eclipse-mosquitto:2.0.1"
 
     Start-Sleep -Milliseconds 2000
 
