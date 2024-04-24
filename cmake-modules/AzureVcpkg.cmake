9474befd21ada175654d8d81ff01a1c2e9b55f9d
Anton Kolesnyk <41349689+antkmsft@users.noreply.github.com>
2024-04-23 17:53:52 -0700

    Update vcpkg SHA to match the C++ repo (#2808)
    Co-authored-by: Anton Kolesnyk <antkmsft@users.noreply.github.com>

--- cmake-modules/AzureVcpkg.cmake
+++ cmake-modules/AzureVcpkg.cmake
@@ -16,7 +16,7 @@ macro(az_vcpkg_integrate)
       message("AZURE_SDK_DISABLE_AUTO_VCPKG is not defined. Fetch a local copy of vcpkg.")
       # GET VCPKG FROM SOURCE
       #  User can set env var AZURE_SDK_VCPKG_COMMIT to pick the VCPKG commit to fetch
-      set(VCPKG_COMMIT_STRING 9854d1d92200d81dde189e53b64c9ba6a305dc9f) # default SDK tested commit
+      set(VCPKG_COMMIT_STRING 8150939b69720adc475461978e07c2d2bf5fb76e) # default SDK tested commit
       if(DEFINED ENV{AZURE_SDK_VCPKG_COMMIT})
         message("AZURE_SDK_VCPKG_COMMIT is defined. Using that instead of the default.")
         set(VCPKG_COMMIT_STRING "$ENV{AZURE_SDK_VCPKG_COMMIT}") # default SDK tested commit
