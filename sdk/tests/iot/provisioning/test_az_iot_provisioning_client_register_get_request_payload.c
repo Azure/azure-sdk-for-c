23454005a43396736a1dc477a60ee6105367ac9b
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2025-07-16 16:00:44 -0700

    Add test for CSR request option

--- sdk/tests/iot/provisioning/test_az_iot_provisioning_client_register_get_request_payload.c
+++ sdk/tests/iot/provisioning/test_az_iot_provisioning_client_register_get_request_payload.c
@@ -28,6 +28,7 @@
 #define TEST_CUSTOM_PAYLOAD "{\"testData\":1, \"testData2\":{\"a\":\"b\"}}"
 #define TEST_ID_SCOPE "0neFEEDC0DE"
 #define TEST_OPERATION_ID "4.d0a671905ea5b2c8.42d78160-4c78-479e-8be7-61d5e55dac0d"
+#define TEST_CSR "this is a mock CSR"
 
 static const az_span test_global_device_hostname
     = AZ_SPAN_LITERAL_FROM_STR("global.azure-devices-provisioning.net");
@@ -235,6 +236,40 @@ static void test_az_iot_provisioning_client_get_request_payload_custom_payload_s
   _az_POP_WARNINGS
 }
 
+static void test_az_iot_provisioning_client_register_get_request_payload_csr_succeed(
+    void** state)
+{
+  (void)state;
+
+  az_iot_provisioning_client client = { 0 };
+  az_result ret = az_iot_provisioning_client_init(
+      &client,
+      test_global_device_hostname,
+      AZ_SPAN_FROM_STR(TEST_ID_SCOPE),
+      AZ_SPAN_FROM_STR(TEST_REGISTRATION_ID),
+      NULL);
+  assert_int_equal(AZ_OK, ret);
+
+  char expected_payload[] = "{\"registrationId\":\"" TEST_REGISTRATION_ID "\", \"csr\":\"" TEST_CSR "\"}";
+  size_t expected_payload_len = sizeof(expected_payload) - 1;
+
+  uint8_t payload[TEST_PAYLOAD_RESERVE_SIZE];
+  memset(payload, 0xCC, sizeof(payload));
+  size_t payload_len = 0xBAADC0DE;
+
+  az_iot_provisioning_client_payload_options options = az_iot_provisioning_client_payload_options_default();
+  options.csr = AZ_SPAN_FROM_STR(TEST_CSR);
+
+  _az_PUSH_IGNORE_DEPRECATIONS
+  ret = az_iot_provisioning_client_register_get_request_payload(
+      &client, AZ_SPAN_EMPTY, &options, payload, sizeof(payload), &payload_len);
+  _az_POP_WARNINGS
+  assert_int_equal(AZ_OK, ret);
+  assert_int_equal(payload_len, expected_payload_len);
+  assert_memory_equal(expected_payload, payload, expected_payload_len);
+  assert_int_equal((uint8_t)0xCC, payload[expected_payload_len]);
+}
+
 int test_az_iot_provisioning_client_register_get_request_payload()
 {
 #ifndef AZ_NO_PRECONDITION_CHECKING
@@ -254,6 +289,7 @@ int test_az_iot_provisioning_client_register_get_request_payload()
 
     cmocka_unit_test(test_az_iot_provisioning_client_get_request_payload_no_custom_payload_succeed),
     cmocka_unit_test(test_az_iot_provisioning_client_get_request_payload_custom_payload_succeed),
+    cmocka_unit_test(test_az_iot_provisioning_client_register_get_request_payload_csr_succeed),
   };
 
   return cmocka_run_group_tests_name("az_iot_provisioning_client_payload", tests, NULL, NULL);
